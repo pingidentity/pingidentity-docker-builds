b718c5242aaaedf78e501e0ff226da8a7dd79a04
arno <arnaud.lacour@gmail.com>
2019-02-15 12:41:44 -0500

    update pingaccess to passthrough entrypoint model

--- pingaccess/entrypoint.sh
+++ pingaccess/entrypoint.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env sh
-# check if an instance has already been run
-if ! test -d /opt/out/instance ; then	
-	# if not, then we deploy the bits to /opt/out
-	cp -rf /opt/server/ /opt/out/instance
+if test "$1" = 'start-server' ; then
+	# check if an instance has already been run
+	if ! test -d /opt/out/instance ; then	
+		# if not, then we deploy the bits to /opt/out
+		cp -rf /opt/server/ /opt/out/instance
 
-	if ! test -z "${SERVER_PROFILE_URL}" ; then
-		# clone the server profile repo if provided
-		git clone ${SERVER_PROFILE_URL} /opt/in
-	fi
+		if ! test -z "${SERVER_PROFILE_URL}" ; then
+			# clone the server profile repo if provided
+			git clone ${SERVER_PROFILE_URL} /opt/server-profile
+			cp -rf /opt/server-profile/* /opt/in
+		fi
 
-	if test -d "/opt/in/instance" ; then
-		cp -rf /opt/in/instance /opt/out/
+		if test -d "/opt/in/instance" ; then
+			cp -rf /opt/in/instance /opt/out/
+		fi
 	fi
+	sh /opt/postStart.sh &
+	tail -F /opt/out/instance/log/pingaccess.log &
+	sh -x /opt/out/instance/bin/run.sh
+else
+	exec "$@"
 fi
-sh /opt/postStart.sh &
-tail -F /opt/out/instance/log/pingaccess.log &
-sh -x /opt/out/instance/bin/run.sh
