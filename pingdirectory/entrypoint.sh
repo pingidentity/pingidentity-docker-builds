19f9031be35ff7246605e711b773e0f164636e45
arno <arnaud.lacour@gmail.com>
2019-02-15 19:30:51 -0500

    add support to checkout specific git branch for pingdirectory

--- pingdirectory/entrypoint.sh
+++ pingdirectory/entrypoint.sh
@@ -16,6 +16,11 @@ if [[ "$1" = 'start-server' ]]; then
     if ! test -z "${SERVER_PROFILE_URL}" ; then
       # deploy configuration if provided
       git clone ${SERVER_PROFILE_URL} /opt/server-profile | tee -a ${LOG_FILE}
+      if ! test -z "${SERVER_PROFILE_BRANCH}" ; then
+        cd /opt/server-profile
+        git checkout ${SERVER_PROFILE_BRANCH}
+        cd -
+      fi
       cp -rf /opt/server-profile/* /opt/in
     fi
 
