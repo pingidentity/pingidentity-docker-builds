34a0d102c3f516e0537643fc514f0a028e8d1c5f
arno <arnaud.lacour@gmail.com>
2019-02-15 19:50:12 -0500

    add support for git branch to pingdelegator

--- pingdelegator/entrypoint.sh
+++ pingdelegator/entrypoint.sh
@@ -2,11 +2,16 @@
 set -ex
 
 if test "${1}" = "start-server" ; then
-	if ! test -z "${SERVER_PROFILE_URL}" ; then
-		# clone server profile if provided
-		git clone ${SERVER_PROFILE_URL} /opt/server-profile
-        cp -rf /opt/server-profile /opt/in
-	fi
+    if ! test -z "${SERVER_PROFILE_URL}" ; then
+      # deploy configuration if provided
+      git clone ${SERVER_PROFILE_URL} /opt/server-profile | tee -a ${LOG_FILE}
+      if ! test -z "${SERVER_PROFILE_BRANCH}" ; then
+        cd /opt/server-profile
+        git checkout ${SERVER_PROFILE_BRANCH}
+        cd -
+      fi
+      cp -rf /opt/server-profile/* /opt/in
+    fi
 
     cp -rf /opt/in/* /usr/share/nginx/html/delegator/
 
