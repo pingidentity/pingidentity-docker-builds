cc0adfa12067b6befd68363f6e6c77c9d52b9cc7
arno <arnaud.lacour@gmail.com>
2019-02-19 16:06:52 -0500

    added support for provided an encryption passphrase to enable topology-wide encryption

--- pingdirectory/entrypoint.sh
+++ pingdirectory/entrypoint.sh
@@ -101,22 +101,27 @@ function check_for_license ()
 
 function setup_server_instance ()
 {
-  certificateOptions="      --generateSelfSignedCertificate"
+  certificateOptions="--generateSelfSignedCertificate"
   if test -f ${SERVER_ROOT_DIR}/config/keystore && test -f ${SERVER_ROOT_DIR}/config/keystore.pin ; then
-    certificateOptions="      --useJavaKeystore ${SERVER_ROOT_DIR}/config/keystore --keyStorePasswordFile ${SERVER_ROOT_DIR}/config/keystore.pin"
+    certificateOptions="--useJavaKeystore ${SERVER_ROOT_DIR}/config/keystore --keyStorePasswordFile ${SERVER_ROOT_DIR}/config/keystore.pin"
   elif test -f ${SERVER_ROOT_DIR}/config/keystore.p12 && test -f ${SERVER_ROOT_DIR}/config/keystore.pin ; then
-    certificateOptions="      --usePkcs12Keystore ${SERVER_ROOT_DIR}/config/keystore --keyStorePasswordFile ${SERVER_ROOT_DIR}/config/keystore.pin"
+    certificateOptions="--usePkcs12Keystore ${SERVER_ROOT_DIR}/config/keystore --keyStorePasswordFile ${SERVER_ROOT_DIR}/config/keystore.pin"
   fi
 
   if test -f ${SERVER_ROOT_DIR}/config/truststore ; then
-    certificateOptions="      --useJavaTruststore ${SERVER_ROOT_DIR}/config/keystore"
+    certificateOptions="--useJavaTruststore ${SERVER_ROOT_DIR}/config/keystore"
   elif test -f ${SERVER_ROOT_DIR}/config/truststore.p12 ; then
-    certificateOptions="      --usePkcs12Truststore ${SERVER_ROOT_DIR}/config/keystore"
+    certificateOptions="--usePkcs12Truststore ${SERVER_ROOT_DIR}/config/keystore"
   fi
   if test -f ${SERVER_ROOT_DIR}/config/truststore.pin ; then
     certificateOptions="${certificateOptions} --trustStorePasswordFile ${SERVER_ROOT_DIR}/config/truststore.pin"
   fi
   certificateOptions="${certificateOptions} --certNickname ${CERTIFICATE_NICKNAME:-server-cert}"
+
+  encryptionArgument="--encryptDataWithRandomPassphrase"
+  if test -f ${ENCRYPTION_PASSWORD_FILE} ; then
+    encryptionArgument="--encryptDataWithPassphraseFromFile ${ENCRYPTION_PASSWORD_FILE}"
+  fi
 
   "${SERVER_ROOT_DIR}"/setup \
     --no-prompt \
@@ -130,6 +135,7 @@ function setup_server_instance ()
     --ldapsPort ${LDAPS_PORT} \
     --httpsPort ${HTTPS_PORT} \
     ${certificateOptions} \
+    ${encryptionArgument} \
     --rootUserDN "${ROOT_USER_DN}" \
     --rootUserPasswordFile "${ROOT_USER_PASSWORD_FILE}" \
     --baseDN "${USER_BASE_DN}" \
