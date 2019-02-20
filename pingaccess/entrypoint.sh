39c5f60a0e0ae2dd6694c1286111500eab0d0648
arno <arnaud.lacour@gmail.com>
2019-02-20 14:14:45 -0500

    common support for apply_server_profile

--- pingaccess/entrypoint.sh
+++ pingaccess/entrypoint.sh
@@ -1,24 +1,13 @@
 #!/usr/bin/env sh
+source /opt/utils.sh
+
 if test "$1" = 'start-server' ; then
 	# check if an instance has already been run
 	if ! test -d /opt/out/instance ; then	
 		# if not, then we deploy the bits to /opt/out
 		cp -af /opt/server/ /opt/out/instance
 
-		if ! test -z "${SERVER_PROFILE_URL}" ; then
-			# clone the server profile repo if provided
-			git clone ${SERVER_PROFILE_URL} /opt/server-profile
-			if ! test -z "${SERVER_PROFILE_BRANCH}" ; then
-				cd /opt/server-profile
-				git checkout ${SERVER_PROFILE_BRANCH}
-				cd -
-			fi
-			cp -af /opt/server-profile/* /opt/in
-		fi
-
-		if test -d "/opt/in/instance" ; then
-			cp -af /opt/in/instance /opt/out/
-		fi
+		apply_server_profile
 	fi
 	sh /opt/postStart.sh &
 	tail -F /opt/out/instance/log/pingaccess.log &
--- pingdatasync/entrypoint.sh
+++ pingdatasync/entrypoint.sh
@@ -1,35 +1,7 @@
 #!/usr/bin/env sh
 set -x
 
-# a function to base the execution of a script upon the presence or absence of a file
-function run_if ()
-{
-	mode=$1
-	shift
-
-	runFile=$1
-
-
-	if test -z "$2" ; then
-    if test "${mode}" = "absent" ; then
-      echo "error, when mode=absent a test file must be provide as a third argument"
-      exit 9
-    fi
-		testFile=$1
-	else
-		testFile=$2
-	fi
-
-	if test $mode = "present" ; then
-		if test -f "${testFile}" ; then
-			sh ${runFile}
-		fi
-	else
-		if ! test -f "${testFile}" ; then
-			sh ${runFile}
-		fi		
-	fi
-}
+source /opt/utils.sh
 
 function apply_tools_properties ()
 {
@@ -64,22 +36,6 @@ END
     --suppressMirroredDataChecks \
     --offline \
     --batch-file ${SERVER_ROOT_DIR}/tmp/config.batch
-}
-
-function apply_server_profile ()
-{
-  if ! test -z "${SERVER_PROFILE_URL}" ; then
-    # deploy configuration if provided
-    git clone ${SERVER_PROFILE_URL} /opt/server-profile
-    die_on_error 78 "Git clone failure" 
-    if ! test -z "${SERVER_PROFILE_BRANCH}" ; then
-      cd /opt/server-profile
-      git checkout ${SERVER_PROFILE_BRANCH}
-      cd -
-    fi
-    cp -af /opt/server-profile/${SERVER_PROFILE_PATH}/* /opt/in
-  fi
-  test -d ${IN_DIR}/instance && cp -af ${IN_DIR}/instance ${OUT_DIR}
 }
 
 function apply_extensions ()
