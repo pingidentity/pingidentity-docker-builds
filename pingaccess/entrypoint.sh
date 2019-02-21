6391228eb7eb2491deb66030a07a6a725c24e866
arno <arnaud.lacour@gmail.com>
2019-02-21 09:01:59 -0500

    parameterize the entrypoint

--- pingaccess/entrypoint.sh
+++ pingaccess/entrypoint.sh
@@ -5,14 +5,14 @@ if test "$1" = 'start-server' ; then
 	run_if present ${IN_DIR}/hooks/00-immediate-startup.sh
 
 	# check if an instance has already been run
-	if ! test -d /opt/out/instance ; then	
+	if ! test -d ${OUT_DIR}/instance ; then	
 		# if not, then we deploy the bits to /opt/out
-		cp -af /opt/server/ /opt/out/instance
+		cp -af /opt/server/ ${OUT_DIR}/instance
 
 		apply_server_profile
 	fi
 
-	cd /opt/out/instance/log
+	cd ${OUT_DIR}/instance/log
 	tail -F ${TAIL_LOG_FILES} &
 	
 	run_if present ${IN_DIR}/hooks/50-before-post-start.sh
@@ -26,10 +26,10 @@ if test "$1" = 'start-server' ; then
 
 	if test -z "${2}" ; then
 		# replace the shell with foreground server
-		exec sh /opt/out/instance/bin/run.sh
+		exec sh ${OUT_DIR}/instance/bin/run.sh
 	else
 		# start server in the background and execute the provided command (useful for self-test)
-		sh /opt/out/instance/bin/run.sh &
+		sh ${OUT_DIR}/instance/bin/run.sh &
 		shift
 		exec "$@"
 	fi
