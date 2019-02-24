56d98f71883e6615f8738f56060d1b5b4e213806
arno <arnaud.lacour@gmail.com>
2019-02-24 17:43:31 -0500

    first commit of the compound build for PingDataSync

--- pingcommon/10-first-time-sequence.sh
+++ pingcommon/10-first-time-sequence.sh
@@ -15,7 +15,7 @@ echo "Initializing server for the first time"
 # if this hook is provided it can be executed early on
 run_if present "${HOOKS_DIR}/11-before-copying-bits.sh"
 
-# lay down the bits from the immutable volume to the runtime volume
+# lay down the bits to the runtime volume
 deploy_server_bits
 
 run_if present "${HOOKS_DIR}/12-before-applying-server-profile.sh"
@@ -27,3 +27,5 @@ run_if present "${HOOKS_DIR}/16-apply-server-profile.sh"
 # environment variables will be provided by the server profile
 # shellcheck disable=SC1090
 test -f "${STAGING_DIR}/env_vars" && . "${STAGING_DIR}/env_vars"
+
+run_if present "${HOOKS_DIR}/18-setup-sequence.sh"
--- pingdatasync/liveness.sh
+++ pingdatasync/liveness.sh
@@ -1,3 +1,2 @@
-#!/bin/sh
-
-ldapsearch -p ${LDAPS_PORT} -Z -X -b "cn=monitor" -s base "(&)"
+#!/user/bin/env sh
+ldapsearch -p ${LDAPS_PORT} -Z -X -b "cn=monitor" -s base "(&)" || exit 1
