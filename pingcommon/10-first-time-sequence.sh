37d6702ad47b7db37020ce25b781592591510c7b
arno <arnaud.lacour@gmail.com>
2019-02-25 14:53:06 -0700

    improve interruption of the sequence in case of error

--- pingcommon/10-first-time-sequence.sh
+++ pingcommon/10-first-time-sequence.sh
@@ -19,13 +19,17 @@ run_if present "${HOOKS_DIR}/11-before-copying-bits.sh"
 deploy_server_bits
 
 run_if present "${HOOKS_DIR}/12-before-applying-server-profile.sh"
+die_on_error 12 "Error running hook 12-before-applying-server-profile.sh"
 
 run_if present "${HOOKS_DIR}/14-get-remote-server-profile.sh"
+die_on_error 14 "Error running hook 14-get-remote-server-profile.sh"
 
 run_if present "${HOOKS_DIR}/16-apply-server-profile.sh"
+die_on_error 16 "Error running hook 16-apply-server-profile.sh"
 
 # environment variables will be provided by the server profile
 # shellcheck disable=SC1090
 test -f "${STAGING_DIR}/env_vars" && . "${STAGING_DIR}/env_vars"
 
 run_if present "${HOOKS_DIR}/18-setup-sequence.sh"
+die_on_error 18 "Error running hook 18-setup-sequence.sh"
