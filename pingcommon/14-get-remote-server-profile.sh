438cb78eba713a202fad5d77b4ab3fe33bd38be4
arno <arnaud.lacour@gmail.com>
2019-02-25 14:53:52 -0700

    improve exit code consistency when interrupting for error

--- pingcommon/14-get-remote-server-profile.sh
+++ pingcommon/14-get-remote-server-profile.sh
@@ -6,12 +6,13 @@
 if ! test -z "${SERVER_PROFILE_URL}" ; then
     # deploy configuration if provided
     git clone "${SERVER_PROFILE_URL}" "${SERVER_PROFILE_DIR}"
-    die_on_error 78 "Git clone failure" 
+    die_on_error 14 "Git clone failure" 
     if ! test -z "${SERVER_PROFILE_BRANCH}" ; then
         # https://github.com/koalaman/shellcheck/wiki/SC2103
         (
         cd "${SERVER_PROFILE_DIR}" || return
         git checkout "${SERVER_PROFILE_BRANCH}"
+        die_on_error 14 "Git checkout failure (bad branch name?)"
         )
     fi
     # shellcheck disable=SC2086
