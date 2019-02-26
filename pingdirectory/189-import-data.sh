21381760d915fcbd222c531cfa36af87ec69a71f
arno <arnaud.lacour@gmail.com>
2019-02-26 09:30:04 -0700

    make the file listing more terse

--- pingdirectory/189-import-data.sh
+++ pingdirectory/189-import-data.sh
@@ -26,7 +26,7 @@ if test -d "${STAGING_DIR}/data" ; then
         for backend in $( find "${STAGING_DIR}/data/" -type f \( -iname \*.ldif -o -iname \*.ldif.gz \) -exec basename {} \; | sed 's/[0-9][0-9]-//' | sed 's/-.*$//' | sort | uniq ) ; do
             filesToImport=""
             # shellcheck disable=SC2086 
-            for ldifFile in $( ls "${STAGING_DIR}"/data/??-${backend}-*.ldif "${STAGING_DIR}"/data/??-${backend}-*.ldif.gz | sort ) ; do
+            for ldifFile in $( ls "${STAGING_DIR}"/data/??-${backend}-*.ldif "${STAGING_DIR}"/data/??-${backend}-*.ldif.gz 2>/dev/null | sort ) ; do
                 filesToImport="${filesToImport} -l ${ldifFile}"
             done
             # shellcheck disable=SC2086
