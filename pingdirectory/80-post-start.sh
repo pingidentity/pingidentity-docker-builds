bd8a764e186f6c20303b777b9a4fa67aacd3e689
arno <arnaudlacour@pingidentity.com>
2019-03-04 13:25:12 -0500

    tease out replication topology repair logic out so the script can be called as a job

--- pingdirectory/80-post-start.sh
+++ pingdirectory/80-post-start.sh
@@ -84,29 +84,8 @@ if dsreplication --no-prompt status \
   exit 0
 fi
 
-####
-# cleanup the replication topology in case it has some defunct servers
-###
-status=$( dsreplication status --script-friendly )
-firstLiveServer=$( echo "${status}" | awk 'BEGIN {s=""} $1~/Server:/{s=$2} $1~/Entries:/ && $2!~/N\/A/{print s;exit 0}' )
-# shellcheck disable=2086,2039
-dsconfig \
-    --no-prompt \
-    --useSSL \
-    --trustAll \
-    --hostname "${firstLiveServer}" \
-    --port ${LDAPS_PORT} \
-    set-global-configuration-prop \
-    --set force-as-master-for-mirrored-data:true    
-for defunctServer in $( echo "${status}" | awk '$0 ~ /^Error on/ {split($3,a,":");print a[1]}' ) ; do
-    remove-defunct-server \
-        --serverInstanceName "${defunctServer}" \
-        --bindDN "${ROOT_USER_DN}" \
-        --bindPasswordFile "${ROOT_USER_PASSWORD_FILE}" \
-        --enableDebug \
-        --globalDebugLevel verbose \
-        --no-prompt
-done
+# the topology might need to be mended before new containers can join
+sh "${STAGING_DIR}/81-repair-topology.sh"
 
 echo "Running dsreplication enable"
 # shellcheck disable=SC2039,SC2086
