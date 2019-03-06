1204407a332c45a3ebc5076632dcf22397ed6d0f
arno <arnaudlacour@pingidentity.com>
2019-03-06 02:41:57 -0500

    fix issue in envsubst template expansion

--- pingcommon/15-expand-templates.sh
+++ pingcommon/15-expand-templates.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env sh
 ${VERBOSE} && set -x
 
+# shellcheck source=/dev/null
+if test -f "${STAGING_DIR}/env_vars" ; then
+    set -o allexport
+    . "${STAGING_DIR}/env_vars"
+    set +o allexport
+fi
+
 # we check if there are templates that we have to run through env subst
 # shellcheck disable=SC2044
 for template in $( find "${STAGING_DIR}/" -type f -iname \*.subst ) ; do 
