2cb8bdbde436be3733e814841b8c055304393021
arno <arnaudlacour@pingidentity.com>
2019-03-06 02:42:26 -0500

    fix liveness issues with not having environment

--- pingdatasync/liveness.sh
+++ pingdatasync/liveness.sh
@@ -1,3 +1,8 @@
-#!/user/bin/env sh
+#!/usr/bin/env sh
+${VERBOSE} && set -x
+
+# shellcheck source=/dev/null
+test -f "${STAGING_DIR}/env_vars" && . "${STAGING_DIR}/env_vars"
+
 # shellcheck disable=SC2086
 ldapsearch -T --terse --suppressPropertiesFileComment -p ${LDAPS_PORT} -Z -X -b "cn=monitor" -s base "(&)" 2>/dev/null || exit 1
--- pingdirectory/liveness.sh
+++ pingdirectory/liveness.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
-set -x
+${VERBOSE} && set -x
+
+# shellcheck source=/dev/null
+test -f "${STAGING_DIR}/env_vars" && . "${STAGING_DIR}/env_vars"
 
 # shellcheck disable=SC2086
 ldapsearch -T --terse --suppressPropertiesFileComment -p ${LDAPS_PORT} -Z -X -b "${USER_BASE_DN}" -s base "(&)" 1.1 2>/dev/null || exit 1
