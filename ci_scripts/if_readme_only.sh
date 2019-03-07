d31d0c14634a3d0014d69526989fcbeb82eba91f
samirgandhi19 <samirgandhi19@gmail.com>
2019-03-07 14:28:08 -0600

    [skip ci]

--- ci_scripts/if_readme_only.sh
+++ ci_scripts/if_readme_only.sh
@@ -6,8 +6,6 @@ CHANGED_FILES=`git diff --name-only master HEAD^`
 ONLY_READMES=True
 MD=".md"
 
-echo $TRAVIS_COMMIT
-
 for CHANGED_FILE in $CHANGED_FILES; do
   if ! [[ $CHANGED_FILE =~ $MD ]]; then
     ONLY_READMES=False
