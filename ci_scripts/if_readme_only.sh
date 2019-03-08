97f900e4fceb913dfffa3dcea82b17bbf3e9c67c
samirgandhi19 <samirgandhi19@gmail.com>
2019-03-08 10:28:31 -0600

    working if readme

--- ci_scripts/if_readme_only.sh
+++ ci_scripts/if_readme_only.sh
@@ -2,31 +2,38 @@
 
 
 #for local, uncomment:
-#CHANGED_FILES=$(git diff --name-only master HEAD^)
+# CHANGED_FILES=$(git diff --name-only master HEAD)
 # echo "edited files: " $CHANGED_FILES
-echo $CI_COMMIT_BEFORE_SHA
-# (: "${CHANGED_FILES?}")
-if ${CI_COMMIT_BEFORE_SHA+"false"}
-then
-   echo "var is unset"
-else 
-  echo "found"
-fi
 
+# for gitlab: 
+echo $CI_COMMIT_BEFORE_SHA
 CHANGED_FILES=$(git diff --name-only "$CI_COMMIT_SHA"  "$CI_COMMIT_BEFORE_SHA")
 echo "CHANGED_FILES: " $CHANGED_FILES
+
 ONLY_READMES=True
 MD="\.md"
+check_if_mds()
+{
+  for CHANGED_FILE in $CHANGED_FILES; do
+    echo $CHANGED_FILE
+    echo "TESTING - ${CHANGED_FILE#*$MD} != ${CHANGED_FILE}"
+    if test "${CHANGED_FILE#*$MD}" = "${CHANGED_FILE}" ; then
+      echo "found non-readme"
+      ONLY_READMES=False
+      break
+    fi
+  done
+}
 
-for CHANGED_FILE in $CHANGED_FILES; do
-  echo $CHANGED_FILE
-  echo "TESTING - ${CHANGED_FILE#*$MD} != ${CHANGED_FILE}"
-  if test "${CHANGED_FILE#*$MD}" = "${CHANGED_FILE}" ; then
-    echo "found non-readme"
-    ONLY_READMES=False
-    break
-  fi
-done
+
+if [ "$CI_COMMIT_BEFORE_SHA" = "0000000000000000000000000000000000000000" ]
+then
+   echo "no previous commit"
+else 
+  echo "found"
+  check_if_mds
+fi
+
 
 if test $ONLY_READMES = True ; then
   echo "Only .md files found, exiting."
