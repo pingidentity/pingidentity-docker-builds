bd4c000cf1162cd77be92cfc7d0f6c707d6990e9
samirgandhi19 <samirgandhi19@gmail.com>
2019-03-08 14:55:38 -0600

    test gitlab

--- pingdirectory/liveness.sh
+++ pingdirectory/liveness.sh
@@ -6,4 +6,3 @@ test -f "${STAGING_DIR}/env_vars" && . "${STAGING_DIR}/env_vars"
 
 # shellcheck disable=SC2086
 ldapsearch -T --terse --suppressPropertiesFileComment -p ${LDAPS_PORT} -Z -X -b "${USER_BASE_DN}" -s base "(&)" 1.1 2>/dev/null || exit 1
-
