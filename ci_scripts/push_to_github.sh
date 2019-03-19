cf43380915bc8fa311e7e31ab9ffa78fd03d9a97
samirgandhi19 <samirgandhi19@gmail.com>
2019-03-19 15:49:30 -0500

    test push from CI

---
+++ ci_scripts/push_to_github.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+cd ~/tmp
+#get current commit
+if [ -d "${docker-builds}" ]; then
+  git clone https://${GITLAB_USER}:${GITLAB_TOKEN}@gitlab.corp.pingidentity.com/devops-program/docker-builds
+  cd docker-builds
+else 
+  rm -rf docker-builds
+  git clone https://${GITHUB}:${GITLAB_TOKEN}@gitlab.corp.pingidentity.com/devops-program/docker-builds
+  cd docker-builds
+fi
+
+git remote add gh_location https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/pingidentity/pingidentity-devops-getting-started.git
+
+#don't put CI stuff on public. 
+git checkout -b CI 
+
+git rm -r ci_scripts/ .gitlab-ci.yml
+
+git push gh_location CI
+
+cd ..
+rm -rf docker-builds
