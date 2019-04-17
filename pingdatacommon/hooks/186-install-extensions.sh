89e3e2a8fd65d5576c148a6692d606186f3d1882
arno <arnaudlacour@pingidentity.com>
2019-04-17 07:48:09 -0400

    Fix for the location from whence extensions are sought

--- pingdatacommon/hooks/186-install-extensions.sh
+++ pingdatacommon/hooks/186-install-extensions.sh
@@ -3,7 +3,7 @@ ${VERBOSE} && set -x
 
 if test -d "${STAGING_DIR}/extensions" && ! test -z "$( ls -A "${STAGING_DIR}/extensions/"*.zip 2>/dev/null )" ; then
     # shellcheck disable=SC2045
-    for extension in $( ls -1 "${IN_DIR}/extensions/"*.zip ) ; do 
+    for extension in $( ls -1 "${STAGING_DIR}/extensions/"*.zip ) ; do 
         "${SERVER_ROOT_DIR}/bin/manage-extension" --install "${extension}" --no-prompt
     done
 fi
