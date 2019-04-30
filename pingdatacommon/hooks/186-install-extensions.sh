64e239baee9ada5ac789327f2d6f2ad903723118
arno <arnaudlacour@pingidentity.com>
2019-04-30 08:03:39 -0400

    provide a mechanism through which extensions can be automatically installed via extensions.ping.directory which avoids the need to commit zip or jar files into repos.

--- pingdatacommon/hooks/186-install-extensions.sh
+++ pingdatacommon/hooks/186-install-extensions.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env sh
 ${VERBOSE} && set -x
 
-if test -d "${STAGING_DIR}/extensions" && ! test -z "$( ls -A "${STAGING_DIR}/extensions/"*.zip 2>/dev/null )" ; then
-    # shellcheck disable=SC2045
-    for extension in $( ls -1 "${STAGING_DIR}/extensions/"*.zip ) ; do 
-        "${SERVER_ROOT_DIR}/bin/manage-extension" --install "${extension}" --no-prompt
-    done
+EXTENSIONS_DIR="${STAGING_DIR}/extensions"
+if test -d "${EXTENSIONS_DIR}" ; then
+    AUTO_INSTALL_FILE="${EXTENSIONS_DIR}/autoinstall.list"
+    if test -f "${AUTO_INSTALL_FILE}" ; then
+        curl https://extensions.ping.directory/installer -o /tmp/installer
+        chmod +x /tmp/installer 
+        grep -v ^# "${AUTO_INSTALL_FILE}"  | while read -r extension ; do
+            if test -n "${extension}" ; then
+                /tmp/installer -I "${SERVER_ROOT_DIR}" -e "${extension}"
+            fi
+        done
+    fi
+
+    if ! test -z "$( ls -A "${STAGING_DIR}/extensions/"*.zip 2>/dev/null )" ; then
+        # shellcheck disable=SC2045
+        for extension in $( ls -1 "${STAGING_DIR}/extensions/"*.zip ) ; do 
+            "${SERVER_ROOT_DIR}/bin/manage-extension" --install "${extension}" --no-prompt
+        done
+    fi
 fi
+
