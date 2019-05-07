ef60072759ae9c69df86f0686d5cd49117b96e5e
samirgandhi <samirgandhi@pingidentity.com>
2019-05-07 10:36:36 -0500

    update gcr repo

--- ci_scripts/deploy_to_gcr.sh
+++ ci_scripts/deploy_to_gcr.sh
@@ -4,6 +4,6 @@ set -x
 product="$1"
 tags=$(docker images "pingidentity/${product}*" --format "{{.Tag}}" -q)
 for tag in $tags ; do
-  docker tag pingidentity/"$product":"$tag" gcr.io/ping-devops-gte/"$product":"$tag"
-  docker push gcr.io/ping-devops-gte/"$product":"$tag"
+  docker tag pingidentity/"$product":"$tag" gcr.io/ping-identity/"$product":"$tag"
+  docker push gcr.io/ping-identity/"$product":"$tag"
 done
