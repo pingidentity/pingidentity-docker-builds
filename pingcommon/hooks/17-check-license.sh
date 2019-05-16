9ba260311eb5d6ed4e295dd9c77b5ace4c3818d4
Terry Sigle <terrysigle@pingidentity.com>
2019-05-16 15:55:03 -0500

    Press in eval license, when devops-user/devops-key not present

--- pingcommon/hooks/17-check-license.sh
+++ pingcommon/hooks/17-check-license.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 ${VERBOSE} && set -x
 
+# echo colorization options
+RED_COLOR='\033[0;31m'
+NORMAL_COLOR='\033[0m'
+
 ##################################################################
 # Check for license file
 #  1. If in SERVER_ROOT_DIR, good
@@ -14,24 +18,46 @@ LICENSE_FILE="${LICENSE_DIR}/${LICENSE_FILE_NAME}"
 
 if test -f "${LICENSE_FILE}" ; then
    licenseFound="true"
-elif ! test -z "${PING_IDENTITY_DEVOPS_USER}" && ! test -z "${PING_IDENTITY_DEVOPS_KEY}" ; then
+else
+   if test -z "${PING_IDENTITY_DEVOPS_USER}" && test -z "${PING_IDENTITY_DEVOPS_KEY}" ; then
+       echo -e "${RED_COLOR}"
+       echo "##################################################################################"
+       echo "############################        ALERT        #################################"
+       echo "##################################################################################"
+       echo "# "
+       echo "# No Ping Identity DevOps User or Key was passed.  Obtaining a generic short term"
+       echo "# eval license."
+       echo "# "
+       echo "# This automatic eval license will be deprecated soon, requiring your own"
+       echo "# personal DevOps User and Key."
+       echo "# "
+       echo "# More info on Product Licenses at:"
+       echo "#      https://pingidentity-devops.gitbook.io/devops/prod-license"
+       echo "# "
+       echo "##################################################################################"
+       echo -e "${NORMAL_COLOR}"
+       PING_IDENTITY_DEVOPS_USER="${PING_IDENTITY_EVAL_USER}"
+       PING_IDENTITY_DEVOPS_KEY="${PING_IDENTITY_EVAL_KEY}"
+    fi
+
     ##################################################################
     # Let's get the license from the license server
     ##################################################################
     if ! test -z "${LICENSE_SHORT_NAME}" && ! test -z "${LICENSE_VERSION}" ; then
-        echo "Pulling evauation license from Ping Identity for ${LICENSE_SHORT_NAME} v${LICENSE_VERSION}"
+        echo "Pulling evauation license from Ping Identity for ${LICENSE_SHORT_NAME} v${LICENSE_VERSION}..."
         
         licenseCurlResult=$( curl -kL -w '%{http_code}' -G \
             --data-urlencode "product=${LICENSE_SHORT_NAME}" \
             --data-urlencode "version=${LICENSE_VERSION}" \
             --data-urlencode "user=${PING_IDENTITY_DEVOPS_USER}" \
             --data-urlencode "devops-key=${PING_IDENTITY_DEVOPS_KEY}" \
-            --data-urlencode "devops-app=docker-image" \
+            --data-urlencode "devops-app=docker-${PING_PRODUCT}-${LICENSE_VERSION}" \
             "https://license.pingidentity.com/devops/licensekey" \
-            -o "${LICENSE_FILE}" )
+            -o "${LICENSE_FILE}" 2> /dev/null )
         if test $licenseCurlResult -eq 200 ; then
             echo "Successfully pulled evaluation license from Ping Identity"
             cat "${LICENSE_FILE}" | sed 's/^/    /'
+            echo ""
             licenseFound="true"
         else
             echo "Unable to download evaluation product.lic (${licenseCurlResult}), most likely due to invalid PING_IDENTITY_DEVOPS_USER/PING_IDENTITY_DEVOPS_KEY"
@@ -43,8 +69,16 @@ elif ! test -z "${PING_IDENTITY_DEVOPS_USER}" && ! test -z "${PING_IDENTITY_DEVO
 fi
 
 if test ! "${licenseFound}" = "true" ; then
-    echo "License File absent"
-    echo "More info on Product Licenses at:"
-    echo "     https://pingidentity-devops.gitbook.io/devops/prod-license"
+    echo -e "${RED_COLOR}"
+    echo "##################################################################################"
+    echo "############################        ALERT        #################################"
+    echo "##################################################################################"
+    echo "# "
+    echo "# License File absent"
+    echo "# More info on Product Licenses at:"
+    echo "#      https://pingidentity-devops.gitbook.io/devops/prod-license"
+    echo "# "
+    echo "################################################################################"
+    echo -e "${NORMAL_COLOR}"
     exit 89
 fi
