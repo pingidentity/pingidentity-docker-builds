c642a90e73f733ae6a35a120288cd5517e585e86
Terry Sigle <terrysigle@pingidentity.com>
2019-05-19 22:55:11 -0500

    Adding echos of git info, added docs and cleaned up dead code

--- pingcommon/hooks/14-get-remote-server-profile.sh
+++ pingcommon/hooks/14-get-remote-server-profile.sh
@@ -9,6 +9,7 @@ getValue ()
     eval printf '%s' "\${${1}}"
 }
 
+# performs a git clone on the server profile passed
 getProfile ()
 {
     serverProfileUrl=$( getValue "${1}_URL" )
@@ -19,34 +20,37 @@ getProfile ()
     rm -rf "${SERVER_PROFILE_DIR}"
     if test -n "${serverProfileUrl}" ; then
         # deploy configuration if provided
+        echo "Getting ${1}"
+        echo "  git url: ${serverProfileUrl}"
+        test -n "${serverProfileBranch}" && echo "   branch: ${serverProfileBranch}"
+        test -n "${serverProfilePath}" && echo "     path: ${serverProfilePath}"
+
         git clone --depth 1 ${serverProfileBranch:+--branch} ${serverProfileBranch} "${serverProfileUrl}" "${SERVER_PROFILE_DIR}"
         die_on_error 141 "Git clone failure"  || exit ${?}
-        # if test -n "${serverProfileBranch}" ; then
-        #     # https://github.com/koalaman/shellcheck/wiki/SC2103
-        #     (
-        #     cd "${SERVER_PROFILE_DIR}" || return
-        #     git checkout "${serverProfileBranch}"
-        #     die_on_error 14 "Git checkout failure (bad branch name?)"
-        #     )
-        # fi
+        
         # shellcheck disable=SC2086
         cp -af ${SERVER_PROFILE_DIR}/${serverProfilePath}/* "${STAGING_DIR}"
         die_on_error 142 "Copy to staging failure"  || exit ${?}
     fi    
 }
 
+# takes the current server profile name and appends _PARENT to the end
+#   Example: SERVER_PROFILE          returns SERVER_PROFILE_PARENT
+#            SERVER_PROFILE_LICENSE  returns SERVER_PROFILE_LICENSE_PARENT
 getParent ()
 {
     echo ${serverProfilePrefix}${serverProfileName:+_}${serverProfileName}"_PARENT"
 }
 
-
-
+########################################################################################
+# main
 serverProfilePrefix="SERVER_PROFILE"
 serverProfileName=""
 serverProfileParent=$( getParent )
 serverProfileList=""
 
+# creates a spaced separated list of server profiles starting with the parent most
+# profile and moving down.
 while test -n "$( getValue ${serverProfileParent} )" ; do
     # echo "Profile parent variable: ${serverProfileParent}"
     serverProfileName=$( getValue ${serverProfileParent} )
@@ -59,20 +63,3 @@ for serverProfileName in ${serverProfileList} ; do
     getProfile "${serverProfilePrefix}_${serverProfileName}"
 done
 getProfile ${serverProfilePrefix}
-
-# if ! test -z "${SERVER_PROFILE_URL}" ; then
-#     # deploy configuration if provided
-#     git clone "${SERVER_PROFILE_URL}" "${SERVER_PROFILE_DIR}"
-#     die_on_error 141 "Git clone failure"  || exit ${?}
-#     if ! test -z "${SERVER_PROFILE_BRANCH}" ; then
-#         # https://github.com/koalaman/shellcheck/wiki/SC2103
-#         (
-#         cd "${SERVER_PROFILE_DIR}" || return
-#         git checkout "${SERVER_PROFILE_BRANCH}"
-#         die_on_error 14 "Git checkout failure (bad branch name?)"
-#         )
-#     fi
-#     # shellcheck disable=SC2086
-#     cp -af ${SERVER_PROFILE_DIR}/${SERVER_PROFILE_PATH}/* "${STAGING_DIR}"
-#     die_on_error 142 "Copy to staging failure"  || exit ${?}
-# fi
