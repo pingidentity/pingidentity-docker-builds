8c5063d8df56480f93e53a81c2fcbbca15be7a62
Terry Sigle <terrysigle@pingidentity.com>
2019-05-22 14:08:59 -0500

    Revolving issues with pingdelegator.  now uses clean nginx.

--- pingdelegator/entrypoint.sh
+++ pingdelegator/entrypoint.sh
@@ -1,22 +1,46 @@
 #!/usr/bin/env sh
-set -ex
 
 if test "${1}" = "start-server" ; then
-    if ! test -z "${SERVER_PROFILE_URL}" ; then
-      # deploy configuration if provided
-      git clone "${SERVER_PROFILE_URL}" /opt/server-profile
-      if ! test -z "${SERVER_PROFILE_BRANCH}" ; then
-        cd /opt/server-profile || exit 99
-        git checkout "${SERVER_PROFILE_BRANCH}"
-        cd -
-      fi
-      cp -af /opt/server-profile/* /opt/in
-    fi
+    ${PINGFEDERATE_PUBLIC_HOSTNAME:=localhost}
+    ${PINGFEDERATE_PUBLIC_PORT:=9031}
+    ${PINGFEDERATE_DELEGATOR_CLIENTID:=dadmin}
 
-    cp -af /opt/in/* /usr/share/nginx/html/delegator/
+    echo "
+##################################################################################
+#               Ping Identity DevOps Delegator Web Application
+##################################################################################
+# 
+#     Configured with the following values.  
+# 
+#         PINGFEDERATE_PUBLIC_HOSTNAME: ${PINGFEDERATE_PUBLIC_HOSTNAME}
+#             PINGFEDERATE_PUBLIC_PORT: ${PINGFEDERATE_PUBLIC_PORT}
+#      PINGFEDERATE_DELEGATOR_CLIENTID: ${PINGFEDERATE_DELEGATOR_CLIENTID}
+# 
+#     To set via a docker run or .yaml just set them using examples below
+#
+#    docker run
+#           ...
+#           --env PINGFEDERATE_PUBLIC_HOSTNAME=myhost.mydomain.com
+#           ...
+#
+#      To use with '.yaml' file (use snippet below)
+#
+#    pingdirectry:
+#       environment: PINGFEDERATE_PUBLIC_HOSTNAME=myhost.mydomain.com
+##################################################################################
+"
 
-    # how do I start nginx ???
-    exec /usr/sbin/nginx -g 'daemon off;'
+    cd /usr/share/nginx/html/delegator
+
+    sed -e "s/PF_HOST = 'localhost'/PF_HOST = '${PINGFEDERATE_PUBLIC_HOSTNAME}'/" \
+        -e "s/PF_PORT = '9031'/PF_PORT = '${PINGFEDERATE_PUBLIC_PORT}'/" \
+        -e "s/DADMIN_CLIENT_ID = 'dadmin'/DADMIN_CLIENT_ID = '${PINGFEDERATE_DELEGATOR_CLIENTID}'/" \
+        "example.config.js" > "config.js"
+
+    chmod 644 "config.js"
+
+    cd /
+    nginx -g 'daemon off;'
 else
     exec "$@"
 fi
