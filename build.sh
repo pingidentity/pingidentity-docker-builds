c6501ab72eb216754d1656f4794e8e936c66ac4c
Terry Sigle <terrysigle@pingidentity.com>
2019-08-01 10:10:46 -0500

    added pingdatagovernance

--- build.sh
+++ build.sh
@@ -110,7 +110,7 @@ errorExit ()
     exit "${errorCode}"
 }
 
-productsToBuild="federate access datasync directory"
+productsToBuild="federate access datasync directory datagovernance"
 OSesToBuild="alpine centos ubuntu"
 #
 # Parse the provided arguments, if any
