186c15070bfeba043a79571efff73f764135e6ab
Terry Sigle <terrysigle@pingidentity.com>
2019-07-31 20:09:52 -0500

    Fixing extension issue with manage-profile setup.  Using skipImports option.  Include env_vars file is available

--- pingdirectory/hooks/183-run-setup.sh
+++ pingdirectory/hooks/183-run-setup.sh
@@ -19,6 +19,7 @@ test -f "${HOOKS_DIR}/pingdata.lib.sh" && . "${HOOKS_DIR}/pingdata.lib.sh"
 if test "${PD_STATE}" == "GENESIS" ; then
     echo "PD_STATE is GENESIS ==> Processing Templates"
     
+    # TODO need to process all ldif subdirectories, not just userRoot
     LDIF_DIR="${PD_PROFILE}/ldif/userRoot"
     TEMPLATE_DIR="${LDIF_DIR}"
     test -z "${MAKELDIF_USERS}" && MAKELDIF_USERS=0
@@ -33,7 +34,24 @@ if test "${PD_STATE}" == "GENESIS" ; then
 else
     echo "PD_STATE is not GENESIS ==> Skipping Templates"
     echo "PD_STATE is not GENESIS ==> Will not process ldif imports"
-    rm -rf "${PD_PROFILE}/ldif/*"
+    _skipImports="--skipImportLdif "
+fi
+
+#
+# Do to an issue with the manage-profile process, we need to install the extensions before we 
+# setup, so the dsconfig command can properly configure the extension.
+# So we will do that with the server root prior to manage-profile
+#
+EXTENSION_DIR="${PD_PROFILE}/server-sdk-extensions"
+if test -d "${EXTENSION_DIR}" ; then
+    echo "Processing extensions in ${EXTENSION_DIR}..."
+    for _extension in $( find "${EXTENSION_DIR}" -mindepth 1 -maxdepth 1 2> /dev/null ) ; do
+        "${SERVER_ROOT_DIR}"/bin/manage-extension \
+            --install "${_extension}" \
+            --no-prompt
+
+        rm -rf "${_extension}"
+    done
 fi
 
 # TODO - See the TODO in pingdata.lib.sh
@@ -55,10 +73,17 @@ jvmOptions=$( getJvmOptions )
 
 export certificateOptions encryptionOption jvmOptions 
 
+# TODO - Maybe allow for an ENV_VARS variable specifying a file (default to "${STAGING_DIR}/env_vars")
+
+if test -f "${STAGING_DIR}/env_vars"  ; then
+    _manageProfileOptions="--profileVariablesFile ${STAGING_DIR}/env_vars "
+fi
+
 # run the manage-profile to setup the server
 "${SERVER_ROOT_DIR}"/bin/manage-profile setup \
     --profile "${PD_PROFILE}" \
-    --profileVariablesFile "${STAGING_DIR}/env_vars" \
     --useEnvironmentVariables \
     --tempProfileDirectory "/tmp" \
-    --doNotStart
+    --doNotStart \
+    ${_manageProfileOptions} \
+    ${_skipImports}
