d188ed257e114c02511f9d32cc1ddc4ae40f3863
Savitha Ganapathi <savithaganapathi@pingidentity.com>
2019-08-21 21:19:54 +0000

    Cleaned up shutdown sequence to take advantage of new features in 7.3
    - The remove-defunct-server tool no longer needs to disable the LDAP connection
      handler or force a master prior to removing a server from the topology. It now
      has the ability to remove an online server with the --ignoreOnline option.
    
    - Also, adding the --retryTimeoutSeconds option to dsreplication
      enable/initialize and remove-defunct-server for internal retries. This
      improves the general robustness of these commands. The default retry timeout
      is set to 2 minutes in the Dockerfile as an (overriable) environment variable.

--- pingdirectory/hooks/90-shutdown-sequence.sh
+++ pingdirectory/hooks/90-shutdown-sequence.sh
@@ -7,89 +7,27 @@ ${VERBOSE} && set -x
 # shellcheck source=../../pingcommon/hooks/pingcommon.lib.sh
 . "${HOOKS_DIR}/pingcommon.lib.sh"
 
-LOG_FILE="${SERVER_ROOT_DIR}/logs/preStop.log"
-TOPOLOGY_FILE="${IN_DIR}/topology.json"
+# shellcheck source=/dev/null
+test -f "${STAGING_DIR}/env_vars" && . "${STAGING_DIR}/env_vars"
+# shellcheck source=pingdirectory.lib.sh
+test -f "${HOOKS_DIR}/pingdirectory.lib.sh" && . "${HOOKS_DIR}/pingdirectory.lib.sh"
 
-if ! test  -f "${TOPOLOGY_FILE}" ; then
-  echo "${TOPOLOGY_FILE} not found" > "${LOG_FILE}"
-  exit 0
-fi
-
-{
-  # shellcheck disable=SC2039
-  echo "Starting preStop script on ${HOSTNAME}"
-  echo "Disabling LDAP connection handler"
-} > "${LOG_FILE}"
-
-# shellcheck disable=SC2039,SC2086
-dsconfig --no-prompt \
-  --useSSL --trustAll \
-  --hostname "${HOSTNAME}" --port ${LDAPS_PORT} \
-  set-connection-handler-prop \
-  --handler-name "LDAP Connection Handler" \
-  --set enabled:false >> "${LOG_FILE}" 2>&1
-
-echo "Forcing one of the servers as master" >> "${LOG_FILE}"
-forcedMaster=
-
-for h in $(grep hostname "${TOPOLOGY_FILE}" | cut -d':' -f 2 | tr -d '", '); do
-  echo "Trying search on ${h}:${LDAPS_PORT}" >> "${LOG_FILE}"
-
-  # shellcheck disable=SC2086
-  if ldapsearch -T --terse --suppressPropertiesFileComment -h "${h}" -p ${LDAPS_PORT} -Z -X -b "" -s base "(&)" 2>/dev/null ; then
-    echo "Forcing server ${h} as master" >> "${LOG_FILE}"
-
-    if dsconfig --no-prompt \
-      --useSSL --trustAll \
-      --hostname "${h}" --port ${LDAPS_PORT} \
-      set-global-configuration-prop \
-      --set force-as-master-for-mirrored-data:true >> "${LOG_FILE}" 2>&1; then
-
-      forcedMaster="${h}"
-      echo "Forced ${h} as master" >> "${LOG_FILE}"
-
-      break
-    fi
-  fi
-done
-
-# Sleep for a couple of seconds for a master to be forced, if required
-sleep 2
-
-test -f "${ROOT_USER_PASSWORD_FILE}"  \
-  && ROOT_PASSWORD_ARGS="--bindPasswordFile ${ROOT_USER_PASSWORD_FILE}" \
-  || ROOT_PASSWORD_ARGS="--bindPassword ${ROOT_USER_PASSWORD}"
-
-echo "Removing local defunct server using ${TOPOLOGY_FILE}" >> "${LOG_FILE}"
-# shellcheck disable=SC2039,SC2086
-remove-defunct-server --serverInstanceName ${HOSTNAME} \
-  --topologyFilePath "${TOPOLOGY_FILE}" \
+# This script will remove the server from the topology for any graceful
+# termination, i.e. scale-down and rolling-update when invoked from a pre-stop
+# hook. Ideally, the server is not removed from the topology in the rolling
+# update case. However, when using an orchestration framework like Kubernetes,
+# the pod that the container is running on has no way of knowing why it's going
+# down unless it can find this information from an external source (e.g. a
+# topology.json file uploaded to an S3 bucket). A topology.json file provided
+# through a config-map mounted volume will not do because that will change the
+# pod spec and re-spin --all-- of the pods unnecessarily, even if the only
+# change to the deployment is a reduced replica count.
+echo "Removing server ${HOSTNAME} from the topology"
+remove-defunct-server --no-prompt \
+  --serverInstanceName "${HOSTNAME}" \
+  --retryTimeoutSeconds ${RETRY_TIMEOUT_SECONDS} \
+  --ignoreOnline \
   --bindDN "${ROOT_USER_DN}" \
-  ${ROOT_PASSWORD_ARGS} \
-  --enableDebug --globalDebugLevel verbose \
-  --no-prompt >> "${LOG_FILE}" 2>&1
-
-if grep "No existing server selected for removal" "${LOG_FILE}"; then
-  echo "Removing local defunct server using config.ldif file" >> "${LOG_FILE}"
-  # shellcheck disable=SC2039,SC2086
-  remove-defunct-server --serverInstanceName "${HOSTNAME}" \
-    --bindDN "${ROOT_USER_DN}" \
-    ${ROOT_PASSWORD_ARGS} \
-    --enableDebug --globalDebugLevel verbose \
-    --no-prompt >> "${LOG_FILE}" 2>&1  
-fi
-
-if test -f "${SERVER_ROOT_DIR}"/config/config.ldif.init ; then
-  echo "Restoring init config" >> "${LOG_FILE}"
-  cp "${SERVER_ROOT_DIR}"/config/config.ldif "${SERVER_ROOT_DIR}"/config/config.ldif.init
-fi
-
-if ! test -z "${forcedMaster}" ; then
-  echo "Unforcing server ${forcedMaster} as master" >> "${LOG_FILE}"
-  # shellcheck disable=SC2086
-  dsconfig --no-prompt \
-    --useSSL --trustAll \
-    --hostname "${forcedMaster}" --port ${LDAPS_PORT} \
-    set-global-configuration-prop \
-    --set force-as-master-for-mirrored-data:false >> "${LOG_FILE}" 2>&1
-fi
+  --bindPasswordFile "${ROOT_USER_PASSWORD_FILE}" \
+  --enableDebug --globalDebugLevel verbose
+echo "Server removal exited with return code: $?"
