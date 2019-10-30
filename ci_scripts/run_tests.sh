1b150f2260f28b9fe68c0d3eec82c5adc574a7c9
samirgandhi <samirgandhi@pingidentity.com>
2019-10-30 11:07:55 -0500

    make run_tests match outputs from build.sh

--- ci_scripts/run_tests.sh
+++ ci_scripts/run_tests.sh
@@ -8,8 +8,8 @@ if test  -f "${product}"/versions; then
   versions=$(grep -v "^#" "${product}"/versions)
   for version in ${versions}; do      
     #test this version of this product
-    TAG=${version}-edge docker-compose -f ./"${product}"/build.test.yml up --exit-code-from sut
-    TAG=${version}-edge docker-compose -f ./"${product}"/build.test.yml down
+    TAG=${version}-alpine-edge docker-compose -f ./"${product}"/build.test.yml up --exit-code-from sut
+    TAG=${version}-alpine-edge docker-compose -f ./"${product}"/build.test.yml down
   done
 else
   TAG=edge docker-compose -f ./"${product}"/build.test.yml up --exit-code-from sut
