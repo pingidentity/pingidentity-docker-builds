8af2911f524836de4ed79a12b5419250b1b6a457
samirgandhi <samirgandhi@pingidentity.com>
2019-11-12 07:30:32 -0600

    better cleanup

--- ci_scripts/docker-cleanup.sh
+++ ci_scripts/docker-cleanup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -x
-
+test ${1} = "full" && full="true"
 docker container stop $(docker container ls -aq)
 docker container rm $(docker container ls -aq)
 docker image prune -f
-# docker rmi -f $(docker image ls --format '{{.Repository}} {{.ID}}' "pingidentity/*" | awk '$1 !~ /(base|common)$/ {print $2}')
 docker volume prune -f
+test $full = "true" && docker rmi -f $(docker image ls --format '{{.Repository}} {{.ID}}' "pingidentity/*" | awk '$1 !~ /(base|common)$/ {print $2}')
+docker image prune -f
 exit 0
