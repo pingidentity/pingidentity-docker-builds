19a43eccaea630095ce0999ca1f05f331f6974f4
Terry Sigle <terrysigle@pingidentity.com>
2020-01-08 13:43:16 +1100

    update of initial pap

--- pingdatagovernancepap/hooks/183-run-setup.sh
+++ pingdatagovernancepap/hooks/183-run-setup.sh
@@ -12,6 +12,7 @@ test -f "${HOOKS_DIR}/pingdata.lib.sh" && . "${HOOKS_DIR}/pingdata.lib.sh"
 
 # shellcheck disable=SC2039,SC2086
 "${SERVER_ROOT_DIR}"/bin/setup demo \
+    --licenseKeyFile "${LICENSE_DIR}/${LICENSE_FILE_NAME}" \
     --port ${HTTPS_PORT} \
     --hostname "${REST_API_HOSTNAME}" \
     --generateSelfSignedCertificate \
