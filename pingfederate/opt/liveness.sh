20c6c3a3038e12e0c70ae1c1cc847bddda4f1475
Matthias Scholz <54108199+MatthiasScholzTW@users.noreply.github.com>
2020-03-04 14:35:19 +0100

    fix: typo on the curl command
    -s: silent
    -S: show-errors
    
    https://curl.haxx.se/docs/manpage.html#-S

--- pingfederate/opt/liveness.sh
+++ pingfederate/opt/liveness.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 URL="https://localhost:${PF_ENGINE_PORT}/pf/heartbeat.ping"
 test "${OPERATIONAL_MODE}" = "CLUSTERED_CONSOLE" && URL="https://localhost:${PF_ADMIN_PORT}/pingfederate/app"
-curl -ssk -o /dev/null "${URL}"
+curl -sSk -o /dev/null "${URL}"
 if test ${?} -ne 0 ; then
     # the health check must return 0 for healthy, 1 otherwise
     # but not any other code so we catch the curl return code and
