24cd0f30fc0e114eba05a1757130634959e4a45d
Arnaud Lacour <arnaudlacour@pingidentity.com>
2020-04-16 04:28:54 +0000

    mid-sprint merge request

--- pingdatacommon/opt/staging/hooks/181-install-extensions.sh
+++ pingdatacommon/opt/staging/hooks/181-install-extensions.sh
@@ -10,19 +10,10 @@ ${VERBOSE} && set -x
 EXTENSIONS_DIR="${STAGING_DIR}/extensions"
 PROFILE_EXTENSIONS_DIR="${PD_PROFILE}/server-sdk-extensions"
 
-if test -d "${EXTENSIONS_DIR}" ; then
-    AUTO_INSTALL_FILE="${EXTENSIONS_DIR}/autoinstall.list"
-    if test -f "${AUTO_INSTALL_FILE}" ; then
-        curl https://extensions.ping.directory/installer -o /tmp/installer
-        chmod +x /tmp/installer 
-        grep -v ^# "${AUTO_INSTALL_FILE}"  | while read -r extension ; do
-            if test -n "${extension}" ; then
-                /tmp/installer -I "${SERVER_ROOT_DIR}" -e "${extension}"
-            fi
-        done
-    fi
-
+if test -d "${EXTENSIONS_DIR}" ; 
+then
     extensionID=0
+    # shellcheck disable=SC2044
     for remoteInstallFile in $(find ${EXTENSIONS_DIR} -type f -name \*remote.list ) ; 
     do
         if test -n "${remoteInstallFile}" && test -f "${remoteInstallFile}" ; then
@@ -31,17 +22,19 @@ if test -d "${EXTENSIONS_DIR}" ; then
                 extensionID=$(( extensionID + 1 ))
                 printf "Extension URL: %s - extension Signature URL: %s - GPG repo: %s - GPG key: %s\n" "${extensionUrl}" "${extensionSignatureUrl}" "${keyServer}" "${keyID}"
                 tmpDir="$( mktemp -d )"
-                ( cd "${tmpDir}" && curl -sSo extension-${extensionID}.zip "${extensionUrl}" )
-                extensionFile=$(ls -1tr "${tmpDir}" | tail -1 )
+                extensionFile="${tmpDir}/extension-${extensionID}.zip"
+                curl -sSLo "${extensionFile}" "${extensionUrl}"
+                # extensionFile=$(ls -1tr "${tmpDir}" | tail -1 )
                 
                 export GNUPGHOME="${tmpDir}"
                 if test -n "${extensionSignatureUrl}" ; then
-                    ( cd "${tmpDir}" && curl -sSLO "${extensionSignatureUrl}" )
-                    extensionSignatureFile=$(ls -1tr "${tmpDir}" | tail -1 )
+                    extensionSignatureFile="${extensionFile}.signature"
+                    curl -sSLo "${extensionSignatureFile}" "${extensionSignatureUrl}"
+                    # extensionSignatureFile=$(ls -1tr "${tmpDir}" | tail -1 )
                     if test -n "${keyServer}" && test -n "${keyID}" ; then
                         signatureMatched=false
                         gpg --batch --keyserver "${keyServer}" --recv-keys "${keyID}"
-                        gpg --batch --verify "${tmpDir}/${extensionSignatureFile}" "${tmpDir}/${extensionFile}"
+                        gpg --batch --verify "${extensionSignatureFile}" "${extensionFile}"
                         if test ${?} -eq 0 ; then
                             signatureMatched=true
                         fi
@@ -67,20 +60,9 @@ if test -d "${EXTENSIONS_DIR}" ; then
                 fi
 
                 mkdir -p "${PROFILE_EXTENSIONS_DIR}"
-                cp "${tmpDir}/${extensionFile}" "${PROFILE_EXTENSIONS_DIR}"/
+                cp "${extensionFile}" "${PROFILE_EXTENSIONS_DIR}"/
+                rm -rf "${tmpDir}"
             done < "${remoteInstallFile}"
         fi
     done
-
-    # FIXME: when all PingData products have been updated to use manage-profile, this block may be removed.
-    # This is required for PingData products that do not use manage-profile yet.
-    if test ! "${PING_PRODUCT}" = 'PingDirectory'; then
-        if test -d "${PROFILE_EXTENSIONS_DIR}" && find "${PROFILE_EXTENSIONS_DIR}" -type f -name \*.zip | read; then
-            # shellcheck disable=SC2045
-            for extension in $(ls -1 "${PROFILE_EXTENSIONS_DIR}/"*.zip); do
-                "${SERVER_ROOT_DIR}/bin/manage-extension" --install "${extension}" --no-prompt
-            done
-        fi
-    fi
 fi
-
