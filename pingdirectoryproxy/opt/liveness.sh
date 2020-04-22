61e96f697cc22ba7f70be9441a9f7effb02b4e6d
Terry Sigle <terry@sigle.com>
2020-04-21 21:40:47 -0500

    Source CONTAINER_ENV instead of env_vars

--- pingdirectoryproxy/opt/liveness.sh
+++ pingdirectoryproxy/opt/liveness.sh
@@ -2,7 +2,7 @@
 ${VERBOSE} && set -x
 
 # shellcheck source=/dev/null
-test -f "${STAGING_DIR}/env_vars" && . "${STAGING_DIR}/env_vars"
+test -f "${CONTAINER_ENV}" && . "${CONTAINER_ENV}"
 
 # shellcheck disable=SC2086
 ldapsearch \
