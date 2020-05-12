4eff7d6f763b57fde4193502f8fdb2daf369cc03
arno <arnaudlacour@pingidentity.com>
2020-05-12 11:52:34 -0400

    closes GDO-245

--- pingaccess/opt/staging/hooks/51-add-engine.sh
+++ pingaccess/opt/staging/hooks/51-add-engine.sh
@@ -11,65 +11,89 @@
 #- For example, enabling replication in PingDirectory, initializing Sync 
 #- Pipes in PingDataSync or issuing admin API calls to PingFederate or PingAccess
 
-# shellcheck source=pingcommon.lib.sh
+# shellcheck source=../../../../pingcommon/opt/staging/hooks/pingcommon.lib.sh
 . "${HOOKS_DIR}/pingcommon.lib.sh"
 
+_out=/tmp/pa.api.request.out
+
+_pa_curl ()
+{
+     _curl \
+        --user "${ROOT_USER}:${PING_IDENTITY_PASSWORD}" \
+        --header "X-Xsrf-Header: PingAccess" \
+        --output ${_out} \
+        "${@}"
+    return ${?}
+}
+
 pahost=${PA_CONSOLE_HOST}
-if [[ ! -z "${OPERATIONAL_MODE}" && "${OPERATIONAL_MODE}" = "CLUSTERED_ENGINE" ]]; then
+if test -n "${OPERATIONAL_MODE}" && "${OPERATIONAL_MODE}" = "CLUSTERED_ENGINE"
+then
     echo "This node is an engine..."
-    while true; do
-    curl -ss --silent -o /dev/null -k https://${pahost}:9000/pa/heartbeat.ping 
-    if ! test $? -eq 0 ; then
-        echo "Adding Engine: Server not started, waiting.."
-        sleep 3
-    else
-        echo "PA started, begin adding engine"
-        break
-    fi
+    while true
+    do
+        _curl https://${pahost}:9000/pa/heartbeat.ping 
+        if test $? -ne 0 ; 
+        then
+            echo "Adding Engine: Server not started, waiting.."
+            sleep 3
+        else
+            echo "PA started, begin adding engine"
+            break
+        fi
     done
 
-
-    _pwCheck=$( curl -ks --write-out %{http_code} --output /dev/null -X GET \
-    -u administrator:${PA_ADMIN_PASSWORD} -H "X-Xsrf-Header: PingAccess" \
-    https://${pahost}:9000/pa-admin-api/v3/users/1 )
-    test ! "${_pwCheck}" -gt 200
-    die_on_error 51 "Connection to admin unsuccessful, check vars PA_ADMIN_PASSWORD and PA_CONSOLE_HOST"
+    _pa_curl \
+        https://${pahost}:9000/pa-admin-api/v3/users/1 \
+        2>/dev/null \
+    || die_on_error 51 "Connection to admin unsuccessful, check vars PING_IDENTITY_PASSWORD and PA_CONSOLE_HOST"
 
     # Get Engine Certificate ID
     echo "Retrieving Key Pair ID from administration API..."
-    keypairid=$( curl -k -u Administrator:"${PA_ADMIN_PASSWORD}" -H "X-Xsrf-Header: PingAccess" https://${pahost}:9000/pa-admin-api/v3/httpsListeners | jq '.items[] | select(.name=="CONFIG QUERY") | .keyPairId' )
+    _pa_curl https://${pahost}:9000/pa-admin-api/v3/httpsListeners
+    test ${?} -ne 200 && die_on_error 51 "Could not retrieve key-pair ID"
+    keypairid=$( jq '.items[] | select(.name=="CONFIG QUERY") | .keyPairId' "${_out}" )
     echo "KeyPairId:"${keypairid}
 
     echo "Retrieving the Key Pair alias..."
-    kpalias=$( curl -k -u Administrator:"${PA_ADMIN_PASSWORD}" -H "X-Xsrf-Header: PingAccess" https://${pahost}:9000/pa-admin-api/v3/keyPairs | jq '.items[] | select(.id=='${keypairid}') | .alias' )
+    _pa_curl https://${pahost}:9000/pa-admin-api/v3/keyPairs
+    test ${?} -ne 200 && die_on_error 51 "Could not retrieve key-pair alias"
+    kpalias=$( jq '.items[] | select(.id=='${keypairid}') | .alias' "${_out}" )
     echo "Key Pair Alias:"${kpalias}
 
     echo "Retrieving Engine Certificate ID..."
-    certid=$( curl -k -u Administrator:"${PA_ADMIN_PASSWORD}" -H "X-Xsrf-Header: PingAccess" https://${pahost}:9000/pa-admin-api/v3/engines/certificates| jq '.items[] | select(.alias=='${kpalias}' and .keyPair==true) | .id' )
+    _pa_curl  https://${pahost}:9000/pa-admin-api/v3/engines/certificates
+    test ${?} -ne 200 && die_on_error 51 "Could not retrieve certificate ID"
+    certid=$( jq '.items[] | select(.alias=='${kpalias}' and .keyPair==true) | .id' "${_out}" )
     echo "Engine Cert ID:"${certid}
 
     echo "Adding new engine"
-    host=`hostname`
-    engineid=$( curl -k -X POST -u Administrator:"${PA_ADMIN_PASSWORD}" -H "X-Xsrf-Header: PingAccess" -d "{
-            \"name\":\"${host}\",
-            \"selectedCertificateId\": ${certid}
-        }" https://${pahost}:9000/pa-admin-api/v3/engines | jq '.id' )
+    host=$( hostname )
+    engineid=$( 
+        curl \
+            --insecure \
+            --request POST \
+            --user "${ROOT_USER}:${PA_ADMIN_PASSWORD}" \
+            --header "X-Xsrf-Header: PingAccess" \
+            --data '{"name":"'"${host}"'", "selectedCertificateId": "'"${certid}"'"}' \
+            https://${pahost}:9000/pa-admin-api/v3/engines | jq '.id' )
 
-    echo "EngineId:"${engineid}
-    set PA_ENGINE_ID=${engineid}
+    echo "EngineId: ${engineid}"
     echo "Retrieving the engine config..."
-    curl -k -X POST -u Administrator:"${PA_ADMIN_PASSWORD}" -H "X-Xsrf-Header: PingAccess" https://${pahost}:9000/pa-admin-api/v3/engines/${engineid}/config -o engine-config.zip
+    curl \
+        --insecure \
+        --request POST \
+        --user "${ROOT_USER}:${PA_ADMIN_PASSWORD}" \
+        --header "X-Xsrf-Header: PingAccess" \
+        --output /tmp/engine-config.zip \
+        https://${pahost}:9000/pa-admin-api/v3/engines/${engineid}/config 
 
     echo "Extracting bootstrap and pa.jwk files to conf folder..."
-    unzip -o engine-config.zip -d ${OUT_DIR}/instance
+    unzip -o /tmp/engine-config.zip -d "${OUT_DIR}/instance"
     # ls -la ${OUT_DIR}instance/conf
     # cat ${OUT_DIR}/instance/conf/bootstrap.properties
-    chmod 400 ${OUT_DIR}/instance/conf/pa.jwk
+    chmod 400 "${OUT_DIR}/instance/conf/pa.jwk"
 
     echo "Cleanup zip.."
-    rm engine-config.zip
+    rm /tmp/engine-config.zip
 fi
-
-
-
-
--- pingaccess/opt/staging/hooks/85-import-configuration.sh
+++ pingaccess/opt/staging/hooks/85-import-configuration.sh
@@ -11,8 +11,9 @@
 #- For example, enabling replication in PingDirectory, initializing Sync 
 #- Pipes in PingDataSync or issuing admin API calls to PingFederate or PingAccess
 
-# shellcheck source=pingcommon.lib.sh
+# shellcheck source=../../../../pingcommon/opt/staging/hooks/pingcommon.lib.sh
 . "${HOOKS_DIR}/pingcommon.lib.sh"
+
 set -e
 echo "INFO: begin importing data.."
 
@@ -24,23 +25,87 @@ echo "INFO: begin importing data.."
 # to check on the status of an import use: 
 # curl -k -v -X GET -u "Administrator:${PA_ADMIN_PASSWORD}" -H "Content-Type: application/json" -H "X-Xsrf-Header: PingAccess" \
 #   https://localhost:9000/pa-admin-api/v3/config/import/workflows/1
+if test -f "${STAGING_DIR}/instance/data/data.json"
+then
+    # curl -ks -X POST -u "Administrator:${PA_ADMIN_PASSWORD}" -H "Content-Type: application/json" -H "X-Xsrf-Header: PingAccess" \
+    # -d @${STAGING_DIR}/instance/data/data.json \
+    # https://localhost:9000/pa-admin-api/v3/config/import/workflows > /dev/null
+    _out="/tmp/import.request.out"
+    _import_http_code=$(
+        curl \
+            --insecure \
+            --silent \
+            --write-out '%{http_code}' \
+            --request POST \
+            --user "${ROOT_USER}:${PA_ADMIN_PASSWORD}" \
+            --header "Content-Type: application/json" \
+            --header "X-Xsrf-Header: PingAccess" \
+            --data @${STAGING_DIR}/instance/data/data.json \
+            --output ${_out} \
+            https://localhost:9000/pa-admin-api/v3/config/import/workflows \
+            2>/dev/null
+    )
 
-curl -ks -X POST -u "Administrator:${PA_ADMIN_PASSWORD}" -H "Content-Type: application/json" -H "X-Xsrf-Header: PingAccess" \
-  -d @${STAGING_DIR}/instance/data/data.json \
-  https://localhost:9000/pa-admin-api/v3/config/import/workflows > /dev/null
+    if ! test "${_import_http_code}" = "200"
+    then
+        echo_red "Import request error: ${_import_http_code}"
+        cat "${_out}" | jq
+        exit 85
+    fi
+    
+    _import_id=$( jq -r .id "${_out}" )
+    _out=/tmp/import.status.out
+    _attempts=300
+    while test ${_attempts} -gt 0
+    do
+        _import_http_code=$(
+            curl \
+                --insecure \
+                --silent \
+                --write-out '%{http_code}' \
+                --request GET \
+                --user "${ROOT_USER}:${PA_ADMIN_PASSWORD}" \
+                --header "Content-Type: application/json" \
+                --header "X-Xsrf-Header: PingAccess" \
+                --output ${_out} \
+                https://localhost:9000/pa-admin-api/v3/config/import/workflows \
+                2>/dev/null
+        )
 
-while true ; do
-  _import_status=$(curl -ks -u "Administrator:${PA_ADMIN_PASSWORD}" -H "Content-Type: application/json" -H "X-Xsrf-Header: PingAccess" https://localhost:9000/pa-admin-api/v3/config/import/workflows | jq '.items[-1].status')
-  case "${_import_status}" in
-    '' | '"In Progress"')
-      test "${VERBOSE}" = "true" && echo "import in progress.."
-      sleep 2 ;;
-    '"Complete"')
-      test "${VERBOSE}" = "true" && echo "Import status: ${_import_status}"
-      break ;;
-    *)
-      echo "Import status: ${_import_status}"
-      echo_red "ERROR: Unsuccessful Import"
-      exit 85 ;;
-  esac
-done
+        if test "${_import_http_code}" = 200
+        then
+            _import_status=$( jq -r '.items[]|select(.id=='${_import_id}')|.status' "${_out}" )
+            case "${_import_status}" in
+                '' | 'In Progress')
+                    echo "import in progress.."
+                    sleep 2
+                ;;
+                Complete)
+                    echo_green "Import done."
+                    _attempts=0
+                ;;
+                Failed)
+                    # clean failure, display error, bail
+                    echo_red "Import failed."
+                    jq -r '.items[]|select(.id==1)|.apiErrorView|.flash[0]' "${_out}"
+                    exit 85
+                ;;
+                *)
+                    # unexpected error
+                    echo_red "Import status: ${_import_status}"
+                    echo_red "ERROR: Unsuccessful Import"
+                    exit 85 
+                ;;
+            esac
+        else
+            echo "There was an error retrieving import status, retrying in 3 seconds (HTTP Code: ${_import_http_code})"
+            # Something is really wrong, retrying at most 3 times
+            if test ${_attempts} -gt 3 
+            then
+                _attempts=3
+            fi
+            sleep 3
+        fi
+        _attempts=$(( _attempts - 1 ))
+    done
+fi
