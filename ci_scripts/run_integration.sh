8aed52ad3d0923b0b58ca4c6be422477b7d3acd5
Arnaud Lacour <arnaudlacour@pingidentity.com>
2020-05-20 11:55:46 +0000

    Arno 2005

--- ci_scripts/run_integration.sh
+++ ci_scripts/run_integration.sh
@@ -2,36 +2,37 @@
 test -n "${VERBOSE}" && set -x
 env
 
-if test -z "${CI_COMMIT_REF_NAME}" ;then
-    # shellcheck disable=SC2046 
-    CI_PROJECT_DIR="$( cd $( dirname "${0}" )/.. || exit 97 ; pwd )"
+if test -z "${CI_COMMIT_REF_NAME}"
+then
+    CI_PROJECT_DIR="$( cd "$( dirname "${0}" )/.." || exit 97 ; pwd )"
     test -z "${CI_PROJECT_DIR}" && echo "Invalid call to dirname ${0}" && exit 97
 fi
 CI_SCRIPTS_DIR="${CI_PROJECT_DIR}/ci_scripts"
-# shellcheck source=../ci_scripts/ci_tools.lib.sh
+# shellcheck source=./ci_tools.lib.sh
 . "${CI_SCRIPTS_DIR}/ci_tools.lib.sh"
 
 _exitCode=""
 
 # shellcheck disable=SC2046
-cd $( dirname "${0}" ) || exit 97
+cd "$( dirname "${0}" )" || exit 97
 export PING_IDENTITY_DEVOPS_USER
 export PING_IDENTITY_DEVOPS_KEY
 _totalStart=$( date '+%s' )
 _resultsFile="/tmp/$$.results"
 _headerPattern=' %-58s| %10s| %10s\n'
 _reportPattern='%-57s| %10s| %10s'
+# shellcheck disable=SC2059
 printf "${_headerPattern}" "TEST" "DURATION" "RESULT" > ${_resultsFile}
-for _test in ${CI_PROJECT_DIR}/integration_tests/${1:-*}.test.yml ;
+for _test in "${CI_PROJECT_DIR}/integration_tests/"${1:-*}.test.yml
 do
     banner "Running ${_test} integration test"
     _start=$( date '+%s' )
-    GIT_TAG=${ciTag} REGISTRY=${FOUNDATION_REGISTRY} docker-compose -f ${_test} up --exit-code-from sut --abort-on-container-exit
+    GIT_TAG=${ciTag} REGISTRY=${FOUNDATION_REGISTRY} docker-compose -f "${_test}" up --exit-code-from sut --abort-on-container-exit
     _returnCode=${?}
     _stop=$( date '+%s' )
     _duration=$(( _stop - _start ))
-    GIT_TAG=${ciTag} REGISTRY=${FOUNDATION_REGISTRY} docker-compose -f ${_test} down
-    if test ${_returnCode} -ne 0 ;
+    GIT_TAG=${ciTag} REGISTRY=${FOUNDATION_REGISTRY} docker-compose -f "${_test}" down
+    if test ${_returnCode} -ne 0
     then
         _result="FAIL"
     else    
---
+++ pingcommon/opt/server/README.md
@@ -0,0 +1,4 @@
+# WARNING
+DO NOT use this location for your runtime.
+This directory is to be treated as an immutable, static, read-only file-system.
+For run-time, use the ${SERVER_ROOT_DIR} environment variable in your scripts to refer to the correct location.
