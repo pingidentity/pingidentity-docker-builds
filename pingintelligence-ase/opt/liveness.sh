cf18b9ac397222ec0bddf86cb50644b65bbd7be9
Erik Ostien <erikostien@pingidentity.com>
2020-10-06 09:22:48 -0600

    Fix Dockerfile and start liveness/smoke test work

--- pingintelligence-ase/opt/liveness.sh
+++ pingintelligence-ase/opt/liveness.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/env sh
+#
+# Ping Identity DevOps - Ping Intelligence Liveness Check
+#
 ${VERBOSE} && set -x
-return 0
+
+_livenessCheckResult=$(
+    curl \
+        --insecure \
+        --silent \
+        --write-out '%{http_code}' \
+        --output /dev/null \
+        --request GET \
+        https://localhost:8010/v4/ase \
+        2>/dev/null
+    )
+
+if test "${_livenessCheckResult}" != "200"
+then
+    exit 1
+fi
+
+exit 0
