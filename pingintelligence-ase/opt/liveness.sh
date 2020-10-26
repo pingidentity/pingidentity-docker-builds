9813354128366799c21ce342a870fbd507959564
arno <arnaudlacour@pingidentity.com>
2020-10-26 17:31:44 -0600

    update the liveness to use built-in liveness check

--- pingintelligence-ase/opt/liveness.sh
+++ pingintelligence-ase/opt/liveness.sh
@@ -3,21 +3,5 @@
 # Ping Identity DevOps - Ping Intelligence Liveness Check
 #
 ${VERBOSE} && set -x
-
-_livenessCheckResult=$(
-    curl \
-        --insecure \
-        --silent \
-        --write-out '%{http_code}' \
-        --output /dev/null \
-        --request GET \
-        https://localhost:8010/v4/ase \
-        2>/dev/null
-    )
-
-if test "${_livenessCheckResult}" != "200"
-then
-    exit 1
-fi
-
-exit 0
+result=$( "${SERVER_ROOT_DIR}/lib/controller" "${SERVER_ROOT_DIR}" api ping )
+test ${?} -eq 0 && test "${result}" = "pong"
