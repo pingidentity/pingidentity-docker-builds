5d36ec4fb3820f88c41b24e05e6d188a71acfa08
arno <arnaudlacour@pingidentity.com>
2020-10-29 22:48:04 -0600

    update liveness to use library

--- pingintelligence/opt/liveness.sh
+++ pingintelligence/opt/liveness.sh
@@ -4,6 +4,12 @@
 #
 ${VERBOSE} && set -x
 
+# shellcheck source=../../pingcommon/opt/staging/hooks/pingcommon.lib.sh
+. "${HOOKS_DIR}/pingcommon.lib.sh"
+
+# shellcheck source=staging/hooks/pingintelligence.lib.sh
+. "${HOOKS_DIR}/pingintelligence.lib.sh"
+
 # This liveness probe checks that everything configued is actually responding appropriately
 # This includes, in this order:
 #   - the HTTPS listener
@@ -11,32 +17,56 @@ ${VERBOSE} && set -x
 #   - the management listener
 #   - the cluster management port
 
-ase_conf="${SERVER_ROOT_DIR}/config/ase.conf"
-cluster_conf="${SERVER_ROOT_DIR}/config/cluster.conf"
+check_available_url ()
+{
+    test -z "${1}" && echo "empty URL" && exit 1
+    _url="${1}"
+    curl -sSk --ipv4 -o /dev/null "${_url}"
+    if test ${?} -eq 0
+    then
+        echo_green "ASE availability confirmed on ${_url}"
+    else
+        echo_red "ASE unavailable at ${_url}"
+        exit 1
+    fi
+}
 
-! test -f "${ase_conf}" && echo "ASE configuration not found" && exit 1
-! test -f "${cluster_conf}" && echo "Clusterr configuration not fond" && exit 1
+check_available_tcp ()
+{
+    test -z "${1}" && echo "empty target" && exit 1
+    _target="${1}"
+    wait-for "${_target}" -t 5
+    if test ${?} -eq 0
+    then
+        echo_green "ASE availability confirmed on tcp ${_target}"
+    else
+        echo_red "ASE unavailable at tcp ${_target}"
+        exit 1
+    fi
+}
 
-health=$( awk -F= '$0~/^enable_ase_health=/{print $2}' "${ase_conf}" )
+set -e
+
+health=$( pi_get_config ase enable_ase_health )
 test ${?} -ne 0 && echo "error parsing ASE configuration" && exit 1
 
 # if configured, check the HTTPS port is live
-https_port=$( awk -F= '$0~/^https_wss_port=/{print $2}' "${ase_conf}" )
+https_port=$( pi_get_config ase https_wss_port )
 test ${?} -ne 0 && echo "error parsing ASE configuration" && exit 1
 if test -n "${https_port}"
 then
     if test "${health}" = "true"
     then
         # ping against health check service on HTTP
-        curl -sSk --ipv4 -o /dev/null "https://localhost:${https_port}/ase" || exit 1
+        check_available_url "https://localhost:${https_port}/ase"
     else
         # simple check if the port is open
-        wait-for "localhost:${https_port}" -t 5 || exit 1
+        check_available_tcp "localhost:${https_port}"
     fi
 fi
 
 # If configured, check on the HTTP too
-http_port=$( awk -F= '$0~/^http_ws_port=/{print $2}' "${ase_conf}" )
+http_port=$( pi_get_config ase http_ws_port )
 test ${?} -ne 0 && echo "error parsing ASE configuration" && exit 1
 
 if test -n "${http_port}"
@@ -44,30 +74,32 @@ then
     if test "${health}" = "true"
     then
         # ping against health check service on HTTP
-        curl -sSk --ipv4 -o /dev/null "http://localhost:${http_port}/ase" || exit 1
+        check_available_url "http://localhost:${http_port}/ase"
     else
         # simple check if the port is open
-        wait-for "localhost:${http_port}" -t 5 || exit 1
+        check_available_tcp "localhost:${http_port}"
     fi
 fi
 
-# let's check if the management port is configured
-management_port=$( awk -F= '$0~/^management_port=/{print $2}' "${ase_conf}" )
+# let's check if the management port is configured (it should always be)
+management_port=$( pi_get_config ase management_port )
 test ${?} -ne 0 && echo "error parsing ASE configuration" && exit 1
 if test -n "${management_port}"
 then
-    wait-for "localhost:${management_port}" -t 5 || exit 1
+    check_available_tcp "localhost:${management_port}"
 fi
 
-cluster_enabled=$( awk -F= '$0~/^enable_cluster=/{print $2}' "${ase_conf}" )
+cluster_enabled=$( pi_get_config ase enable_cluster )
 test ${?} -ne 0 && echo "error parsing ASE configuration" && exit 1
 
-cluster_port=$( awk -F= '$0~/^cluster_manager_port=/{print $2}' "${cluster_conf}" )
-test ${?} -ne 0 && echo "error parsing cluster rconfiguration" && exit 1
+cluster_port=$( pi_get_config cluster cluster_manager_port )
+test ${?} -ne 0 && echo "error parsing cluster configuration" && exit 1
 if test "${cluster_enabled}" = "true" && test -n "${cluster_port}"
 then
-    wait-for "localhost:${cluster_port}" -t 5 || exit 1
+    check_available_tcp "localhost:${cluster_port}" -t 5
 fi
+
+test isASERunning || exit 1
 
 # ASE is firing on all cylinders!
 exit 0
