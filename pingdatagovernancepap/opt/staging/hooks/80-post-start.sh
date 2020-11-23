550cde98a5c9fd2b0b5f1fee74ef507f76440b02
Osha Fuangkasae <oshafuangkasae@pingidentity.com>
2020-11-23 16:19:58 +0000

    Update pingdatagovernancepap for 8.2.0.0-GA
    This refactors the pingdatagovernancepap version check into a
    reusable function, and adds logic for a new required argument in `setup`,
    which requires an `--adminPort`. It adds documentation to the Dockerfile
    on how to modify default backup values. Finally, it updates the liveness.sh check to
    use the /healthcheck endpoint when the admin connector is listening.
    
    Reviewer: Jacob Childress
    
    JiraIssue: DS-43341
    
    Product: broker

--- pingdatagovernancepap/opt/staging/hooks/80-post-start.sh
+++ pingdatagovernancepap/opt/staging/hooks/80-post-start.sh
@@ -8,6 +8,9 @@
 # shellcheck source=../../../../pingcommon/opt/staging/hooks/pingcommon.lib.sh
 . "${HOOKS_DIR}/pingcommon.lib.sh"
 
+# shellcheck source=../../../../pingdatacommon/opt/staging/hooks/pingdatacommon.lib.sh
+. "${HOOKS_DIR}/pingdata.lib.sh"
+
 # shellcheck source=pingdatagovernancepap.lib.sh
 test -f "${HOOKS_DIR}/pingdatagovernancepap.lib.sh" && . "${HOOKS_DIR}/pingdatagovernancepap.lib.sh"
 
@@ -16,8 +19,14 @@ if use_oidc_mode ; then
   exit
 fi
 
+if is_version_ge "8.2.0.0-GA" ;
+then
+  _url=localhost:${PING_ADMIN_PORT:-8444}/healthcheck
+else
+  _url=localhost:${HTTPS_PORT}
+fi
 echo "INFO: waiting for PingDataGovernance-PAP to start before importing configuration"
 # shellcheck disable=SC2086
-wait-for localhost:${HTTPS_PORT} -t 200 --  echo "pap running"
+wait-for ${_url} -t 200 --  echo "pap running"
 
 run_hook 81-install-policies.sh
