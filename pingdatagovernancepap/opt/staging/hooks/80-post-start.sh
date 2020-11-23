e2c16d87a63d26d9cb41d4e2885df6c9b113555e
Terry Sigle <terry@sigle.com>
2020-11-23 14:23:29 -0600

    gdo-599 using 127.0.0.1 rather than localhost due to nc (busybox) support

--- pingdatagovernancepap/opt/staging/hooks/80-post-start.sh
+++ pingdatagovernancepap/opt/staging/hooks/80-post-start.sh
@@ -19,14 +19,16 @@ if use_oidc_mode ; then
   exit
 fi
 
+
+# using 127.0.0.1 (rather than localhost) until nc (part ob busybox) supports ipv4/ipv6
 if is_version_ge "8.2.0.0-GA" ;
 then
-  _url=localhost:${PING_ADMIN_PORT:-8444}/healthcheck
+  _url=127.0.0.1:${PING_ADMIN_PORT:-8444}/healthcheck
 else
-  _url=localhost:${HTTPS_PORT}
+  _url=127.0.0.1:${HTTPS_PORT}
 fi
 echo "INFO: waiting for PingDataGovernance-PAP to start before importing configuration"
-# shellcheck disable=SC2086
-wait-for ${_url} -t 200 --  echo "pap running"
+
+wait-for "${_url}" -t 200 --  echo "pap running"
 
 run_hook 81-install-policies.sh
