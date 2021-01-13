c1799288f5cefc974c99ff4d177ee5dde69de5b6
Osha Fuangkasae <oshafuangkasae@pingidentity.com>
2021-01-13 20:36:32 +0000

    Replace wait-for call in pingdatagovernancepap
    This fixes a problem in pingdatagovernancepap 80-post-start.sh where
    wait-for was being used with a URL instead of a hostname and port,
    causing the image to wait for the 200 second timeout to proceed with
    loading policies.
    
    Reviewer: Jacob Childress
    
    JiraIssue: DS-43803
    
    Product: broker

--- pingdatagovernancepap/opt/staging/hooks/80-post-start.sh
+++ pingdatagovernancepap/opt/staging/hooks/80-post-start.sh
@@ -19,16 +19,21 @@ if use_oidc_mode ; then
   exit
 fi
 
-
-# using 127.0.0.1 (rather than localhost) until nc (part ob busybox) supports ipv4/ipv6
-if is_version_ge "8.2.0.0-GA" ;
-then
-  _url=127.0.0.1:${PING_ADMIN_PORT:-8444}/healthcheck
-else
-  _url=127.0.0.1:${HTTPS_PORT}
-fi
 echo "INFO: waiting for PingDataGovernance-PAP to start before importing configuration"
 
-wait-for "${_url}" -t 200 --  echo "pap running"
+# The 8.2.0.0-GA release exposes a new healthcheck endpoint using a self-signed certificate,
+# which requires something other than wait-for to test. Try 5 times to request it.
+if is_version_ge "8.2.0.0-GA" ;
+then
+  _tries=5
+  while test ${_tries} -gt 0 && \
+      ! _curl --insecure "https://127.0.0.1:${PING_ADMIN_PORT:-8444}/healthcheck" 2>/dev/null ; do
+    sleep 3
+    _tries=$(( _tries - 1 ))
+  done
+else
+  # using 127.0.0.1 (rather than localhost) until nc (part ob busybox) supports ipv4/ipv6
+  wait-for -h "127.0.0.1" -p "${HTTPS_PORT}" -t 200 --  echo "pap running"
+fi
 
 run_hook 81-install-policies.sh
