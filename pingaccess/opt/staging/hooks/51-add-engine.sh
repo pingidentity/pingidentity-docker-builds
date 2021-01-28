904e951490542ec10eb3abef7e79962f2409c9ad
Terry Sigle <terry@sigle.com>
2021-01-28 11:54:36 -0600

    gdo-677 Support spaces in certificate aliases and general shellcheck cleanup

--- pingaccess/opt/staging/hooks/51-add-engine.sh
+++ pingaccess/opt/staging/hooks/51-add-engine.sh
@@ -2,20 +2,24 @@
 #
 # Ping Identity DevOps - Docker Build Hooks
 #
-#- This script is started in the background immediately before 
+#- This script is started in the background immediately before
 #- the server within the container is started
 #-
 #- This is useful to implement any logic that needs to occur after the
 #- server is up and running
 #-
-#- For example, enabling replication in PingDirectory, initializing Sync 
+#- For example, enabling replication in PingDirectory, initializing Sync
 #- Pipes in PingDataSync or issuing admin API calls to PingFederate or PingAccess
 
 # shellcheck source=../../../../pingcommon/opt/staging/hooks/pingcommon.lib.sh
 . "${HOOKS_DIR}/pingcommon.lib.sh"
 
-_out=/tmp/pa.api.request.out
-pahost=${PA_CONSOLE_HOST}
+_out="/tmp/pa.api.request.out"
+_configzip="/tmp/engine-config.zip"
+# The environment variables PA_ADMIN_PRIVATE_... are automatically created from
+# ping-devops helm charts
+_pahost=${PA_ADMIN_PRIVATE_HOSTNAME:-PA_CONSOLE_HOST}
+_paport=${PA_ADMIN_PRIVATE_PORT_CLUSTERCONFIG:-PA_ADMIN_PORT}
 _password=${PING_IDENTITY_PASSWORD:-PA_ADMIN_PASSWORD_INITIAL}
 
 _pa_curl ()
@@ -32,10 +36,12 @@ _pa_curl ()
 if test -n "${OPERATIONAL_MODE}" && test "${OPERATIONAL_MODE}" = "CLUSTERED_ENGINE"
 then
     echo "This node is an engine..."
+    echo "Checking admin heartbeat: https://${_pahost}:${_paport}/pa/heartbeat.ping"
+
     while true
     do
-        _pa_curl https://${pahost}:${PA_ADMIN_PORT}/pa/heartbeat.ping
-        if test $? -ne 0 ; 
+        _pa_curl "https://${_pahost}:${_paport}/pa/heartbeat.ping"
+        if test $? -ne 0 ;
         then
             echo "Adding Engine: Server not started, waiting.."
             sleep 3
@@ -46,39 +52,41 @@ then
     done
 
     _pa_curl \
-        https://${pahost}:${PA_ADMIN_PORT}/pa-admin-api/v3/users/1 \
+        "https://${_pahost}:${_paport}/pa-admin-api/v3/users/1" \
         2>/dev/null \
     || die_on_error 51 "Connection to admin unsuccessful, check vars PING_IDENTITY_PASSWORD and PA_CONSOLE_HOST"
 
     # Get Engine Certificate ID
     echo "Retrieving Key Pair ID from administration API..."
-    _pa_curl https://${pahost}:${PA_ADMIN_PORT}/pa-admin-api/v3/httpsListeners
+    _pa_curl "https://${_pahost}:${_paport}/pa-admin-api/v3/httpsListeners"
     test ${?} -ne 200 && die_on_error 51 "Could not retrieve key-pair ID"
     keypairid=$( jq '.items[] | select(.name=="CONFIG QUERY") | .keyPairId' "${_out}" )
-    echo "KeyPairId:"${keypairid}
+    echo "KeyPairId: ${keypairid}"
 
     echo "Retrieving the Key Pair alias..."
-    _pa_curl https://${pahost}:${PA_ADMIN_PORT}/pa-admin-api/v3/keyPairs
+    _pa_curl "https://${_pahost}:${_paport}/pa-admin-api/v3/keyPairs"
     test ${?} -ne 200 && die_on_error 51 "Could not retrieve key-pair alias"
     kpalias=$( jq '.items[] | select(.id=='${keypairid}') | .alias' "${_out}" )
-    echo "Key Pair Alias:"${kpalias}
+    echo "Key Pair Alias: ${kpalias}"
 
     echo "Retrieving Engine Certificate ID..."
-    _pa_curl  https://${pahost}:${PA_ADMIN_PORT}/pa-admin-api/v3/engines/certificates
+    _pa_curl  "https://${_pahost}:${_paport}/pa-admin-api/v3/engines/certificates"
     test ${?} -ne 200 && die_on_error 51 "Could not retrieve certificate ID"
-    certid=$( jq '.items[] | select(.alias=='${kpalias}' and .keyPair==true) | .id' "${_out}" )
-    echo "Engine Cert ID:"${certid}
+    # Escaped double-quotes are used below to handle aliases that contain spaces
+    certid=$( jq ".items[] | select(.alias==\"${kpalias}\" and .keyPair==true) | .id" "${_out}" )
+
+    echo "Engine Cert ID: ${certid}"
 
     echo "Adding new engine"
     host=$( hostname )
-    engineid=$( 
+    engineid=$(
         curl \
             --insecure \
             --request POST \
             --user "${ROOT_USER}:${_password}" \
             --header "X-Xsrf-Header: PingAccess" \
             --data '{"name":"'"${host}"'", "selectedCertificateId": "'"${certid}"'"}' \
-            https://${pahost}:${PA_ADMIN_PORT}/pa-admin-api/v3/engines | jq '.id' )
+            "https://${_pahost}:${_paport}/pa-admin-api/v3/engines" | jq '.id' )
 
     echo "EngineId: ${engineid}"
     echo "Retrieving the engine config..."
@@ -87,15 +95,15 @@ then
         --request POST \
         --user "${ROOT_USER}:${_password}" \
         --header "X-Xsrf-Header: PingAccess" \
-        --output /tmp/engine-config.zip \
-        https://${pahost}:${PA_ADMIN_PORT}/pa-admin-api/v3/engines/${engineid}/config
+        --output "${_configzip}" \
+        "https://${_pahost}:${_paport}/pa-admin-api/v3/engines/${engineid}/config"
 
     echo "Extracting bootstrap and pa.jwk files to conf folder..."
-    unzip -o /tmp/engine-config.zip -d "${OUT_DIR}/instance"
+    unzip -o "${_configzip}" -d "${OUT_DIR}/instance"
     # ls -la ${OUT_DIR}instance/conf
     # cat ${OUT_DIR}/instance/conf/bootstrap.properties
     chmod 400 "${OUT_DIR}/instance/conf/pa.jwk"
 
     echo "Cleanup zip.."
-    rm /tmp/engine-config.zip
+    rm "${_configzip}"
 fi
