ec2cdf8a0eedb1dc8bf2fa1fc3b7c2ef862bdcfe
gmorgan-ping <gmorgan@pingidentity.com>
2021-03-26 12:17:20 -0300

    Testing github actions

--- ci_scripts/deploy_docs.sh
+++ ci_scripts/deploy_docs.sh
@@ -66,7 +66,7 @@ append_footer ()
     append_doc "---"
     test -n "${_srcFile}" && append_doc "This document is auto-generated from _[${_srcFile}](https://github.com/pingidentity/pingidentity-docker-builds/blob/master/${_srcFile})_"
     append_doc ""
-    append_doc "Copyright (c) 2021 Ping Identity Corporation. All rights reserved."
+    append_doc "Copyright © 2021 Ping Identity Corporation. All rights reserved."
 }
 
 #
