3362175ccedf6c6e1bdcfd730ff0f054ab746f90
Arnaud Lacour <arnaudlacour@pingidentity.com>
2021-06-16 17:19:42 +0000

    Resolve GDO-857 "Jmeter heap percentage"

--- apache-jmeter/opt/server/bin/run.sh
+++ apache-jmeter/opt/server/bin/run.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env sh
-set -x
-heap=$( awk '$1~/^MemFree:$/{m=1024;if ($3~/^kB$/){m=1} mem=int(0.9*$2/(1024*m)); if (mem<384){mem=384;} print mem;}' /proc/meminfo )
-if test "${MAX_HEAP_SIZE}" = "AUTO"
-then
-    jvmArgs="-Xmx${heap}m -Xms${heap}m"
-else
-    jvmArgs="-Xmx${MAX_HEAP_SIZE} -Xms${MAX_HEAP_SIZE}"
-fi
+test "${VERBOSE}" = "true" && set -x
+jvmArgs="-XshowSettings:vm -XX:+UseContainerSupport -XX:InitialRAMPercentage=${JAVA_RAM_PERCENTAGE} -XX:MinRAMPercentage=${JAVA_RAM_PERCENTAGE} -XX:MaxRAMPercentage=${JAVA_RAM_PERCENTAGE}"
 localIP=$( ifconfig eth0 | awk '$1~/inet$/ {split($2,ip,":");print ip[2]}' )
 jmeterArgs=" -Djava.rmi.server.hostname=${localIP} -Dserver.rmi.ssl.disable=true -Djmeter.logfile=/var/log/jmeter.log -Dprometheus.ip=0.0.0.0"
 # Word-Split is expected behavior for $jvmArgs and $jmeterArgs. Disable shellcheck.
