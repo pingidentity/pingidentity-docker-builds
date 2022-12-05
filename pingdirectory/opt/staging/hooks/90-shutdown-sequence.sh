561d34161d42da3e847f5ed243f3edac8379c2b8
Henry Recker <henryrecker@pingidentity.com>
2022-12-05 17:05:02 +0000

    Don't remove seed server in 90-shutdown-sequence
    Closes BRASS-570

--- pingdirectory/opt/staging/hooks/90-shutdown-sequence.sh
+++ pingdirectory/opt/staging/hooks/90-shutdown-sequence.sh
@@ -15,4 +15,15 @@ test "${VERBOSE}" = "true" && set -x
 # shellcheck source=./pingdirectory.lib.sh
 test -f "${HOOKS_DIR}/pingdirectory.lib.sh" && . "${HOOKS_DIR}/pingdirectory.lib.sh"
 
-removeDefunctServer
+if test "$(toLower "${SKIP_SHUTDOWN_SEQUENCE}")" = "true"; then
+    echo "SKIP_SHUTDOWN_SEQUENCE environment variable set to true, exiting"
+    exit 0
+fi
+
+# Check for a marker file indicating that this server is the seed server
+if test -f /tmp/seed-server; then
+    echo "remove-defunct-server will not be run on shutdown, since this server is the seed server."
+else
+    echo "Running remove-defunct-server on shutdown."
+    removeDefunctServer
+fi
