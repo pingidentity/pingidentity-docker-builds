c36c71ab69162bd601a8c65977ec67150fb1cf6c
Erik Ostien <erikostien@pingidentity.com>
2023-02-24 14:45:50 +0000

    BRASS-776: Build and Release PF 11.2.3 and 11.1.6
    - Update PingFederate image to version 11.2.3 and 11.1.6. EOL
    11.2.2 and 11.1.5.
    - Update Apache Tomcat to 9.0.72 and EOL 9.0.71
    - Update Alpine OS to 3.17.2 and EOL 3.17.1
    
    Closes BRASS-776

--- ci_scripts/run_helm_integration.sh
+++ ci_scripts/run_helm_integration.sh
@@ -52,7 +52,7 @@ $(cd "${_integration_helm_tests_dir}" && find ./* -type d -maxdepth 1 | sed 's/^
 
     --image-tag-shim {shim-name}
         Tests for a specific image shim tag.
-        Example: alpine:3.17.1
+        Example: alpine:3.17.2
 
     --verbose
         Turn up the volume
