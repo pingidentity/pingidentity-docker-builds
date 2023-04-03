bd9aec6bfa6cc600bbb8aa7b24f257fe4adf0116
Erik Ostien <erikostien@pingidentity.com>
2023-04-03 16:23:58 +0000

    BRASS-858: Update Product Versions and Dependencies
    - Build PingAccess 7.3.0-Beta
    - PingAccess 7.2.0 → 7.2.1
    - PingData 9.1.0.1 → 9.1.0.2
    - Alpine 3.17.2 → 3.17.3
    
    Closes BRASS-858

--- ci_scripts/build_product.sh
+++ ci_scripts/build_product.sh
@@ -244,7 +244,7 @@ for _version in ${versionsToBuild}; do
                 --build-arg IMAGE_VERSION="${imageVersion}" \
                 --build-arg IMAGE_GIT_REV="${GIT_REV_MED}" \
                 --build-arg LICENSE_VERSION="${licenseVersion}" \
-                --build-arg LATEST_ALPINE_VERSION="3.17.2" \
+                --build-arg LATEST_ALPINE_VERSION="3.17.3" \
                 ${VERBOSE:+--build-arg VERBOSE="true"} \
                 ${PING_IDENTITY_SNAPSHOT:+--build-arg PING_IDENTITY_GITLAB_TOKEN="${PING_IDENTITY_GITLAB_TOKEN}"} \
                 ${PING_IDENTITY_SNAPSHOT:+--build-arg SNAPSHOT_URL="${snapshot_url}"} \
