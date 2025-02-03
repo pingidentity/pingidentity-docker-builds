57911a0daf3476bacc0cae07ab22b2cea03ad55f
Erik Ostien <erikostien@pingidentity.com>
2025-02-03 22:13:46 +0000

    PDI-2184: Update Products and Dependencies
    - PingFederate 12.1.4 -> 12.1.5
    - PingData 10.2.0.0 -> 10.2.0.1
    - Alpine 3.21.0 -> 3.21.2
    - Redhat UBI9-Minimal 9.5-1734497536 -> 9.5-1736404155
    - Liberica JDK17 17.0.13+12 -> 17.0.14+10
    
    Closes PDI-2184

--- ci_scripts/build_custom_image.sh
+++ ci_scripts/build_custom_image.sh
@@ -19,7 +19,7 @@ Usage: ${0} {options}
         Ex: pingfederate
     -o, --os-shim
         Required. The docker image tagged name of the operating system for which to build a custom docker image.
-        Ex: alpine:3.21.0
+        Ex: alpine:3.21.2
     -j, --jvm
         Required. The id of the jvm to build
         Ex: al11
