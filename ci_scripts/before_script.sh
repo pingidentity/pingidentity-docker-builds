09defb63690c11b35f476b2f874be604c6e42651
Erik Ostien <erikostien@pingidentity.com>
2025-02-28 18:25:53 +0000

    PDI-2191: Update Docker-Builds Vault Integration Method for GitLab17
    * Move Vault secret retrieval to before_script.sh and write secret to global pipeline var to be used in ci_tools.lib.sh
    * Use ID token to authenticate with vault instead of CI_JOB_JWT
    
    Closes PDI-2191

--- ci_scripts/before_script.sh
+++ ci_scripts/before_script.sh
@@ -34,3 +34,18 @@ type git
 git --version
 type notary
 notary version
+
+# Do not output Vault Secrets or token to logs
+set +xe
+
+#Retreive the vault token to authenticate for vault secrets
+VAULT_TOKEN="$(vault write -field=token auth/jwt_v2/login role=pingdevops jwt="${VAULT_ID_TOKEN}")"
+test -z "${VAULT_TOKEN}" && VAULT_TOKEN="$(vault write -field=token auth/jwt_v2/login role=pingdevops-tag jwt="${VAULT_ID_TOKEN}")"
+test -z "${VAULT_TOKEN}" && echo "Error: Vault token was not retrieved" && exit 1
+export VAULT_TOKEN
+
+#Retreive the vault secret
+vault kv get -mount=pingdevops -field=Signing_Key_Base64 Base64_key > "${VAULT_SECRET_FILE}"
+test $? -ne 0 && echo "Error: Failed to retrieve private docker keys from vault" && exit 1
+
+return 0
