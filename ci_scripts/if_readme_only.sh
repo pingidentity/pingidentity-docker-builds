3f8f44310b4d8114d254df67ca202efad2a6d4f2
David Ross <davidross@pingidentity.com>
2025-06-24 16:18:45 +0000

    Closes PDI-2230, fixing minor typo in Dockerfiles

--- ci_scripts/if_readme_only.sh
+++ ci_scripts/if_readme_only.sh
@@ -18,29 +18,8 @@ sudo chown -R gitlab-runner:gitlab-runner .
 CHANGED_FILES=$(git diff --name-only "${CI_COMMIT_BEFORE_SHA}" "${CI_COMMIT_SHA}")
 echo "CHANGED_FILES:  ${CHANGED_FILES}"
 
-ONLY_READMES="True"
-MD="\.md"
-
-check_if_mds() {
-    for CHANGED_FILE in ${CHANGED_FILES}; do
-        echo "${CHANGED_FILE}"
-        echo "TESTING - ${CHANGED_FILE#*"${MD}"} != ${CHANGED_FILE}"
-        if test "${CHANGED_FILE#*"${MD}"}" = "${CHANGED_FILE}"; then
-            echo "found non-readme"
-            ONLY_READMES="False"
-            break
-        fi
-    done
-}
-
 if test "${CI_COMMIT_BEFORE_SHA}" = "0000000000000000000000000000000000000000"; then
     echo "no previous commit."
-else
-    check_if_mds
-    if test "${ONLY_READMES}" = "True"; then
-        echo "Only Markdown files found, shunting build."
-        exit 1
-    fi
 fi
 
 echo "All checks cleared. Proceeding with build..."
